7a870ab1cab4b07fb905fc0eab41bb27747d069b
cowlol <cowlol@users.sourceforge.net>
2007-10-05 22:37:54 +0000

    First version of the Bitmap class, as well updating the makefile and making small changes here and there.

--- Makefile
+++ Makefile
@@ -7,16 +7,18 @@ LDFLAGS = `sdl-config --libs` -lSDL_image
 #Usually SDL's LDFLAGS -L/usr/lib -lSDL -lpthread -lSDL_image -g
 
 
-all: main.o tools.o map.o chipset.o
-	$(CC) main.o tools.o map.o chipset.o -o easyRPGPlayer $(LDFLAGS)
-main.o: tools.h map.h
+all: main.o tools.o map.o chipset.o bitmap.o
+	$(CC) main.o tools.o map.o chipset.o bitmap.o -o easyRPGPlayer $(LDFLAGS)
+main.o: tools.h map.h bitmap.h main.cpp
 	$(CC) $(CFLAGS) -c main.cpp
-tools.o: tools.h
+tools.o: tools.h tools.cpp
 	$(CC) $(CFLAGS) -c tools.cpp
-map.o: map.h tools.h
+map.o: map.h tools.h map.cpp
 	$(CC) $(CFLAGS) -c map.cpp
-chipset.o: chipset.h tools.h
+chipset.o: chipset.h tools.h chipset.cpp
 	$(CC) $(CFLAGS) -c chipset.cpp
+bitmap.o: bitmap.h tools.h bitmap.cpp
+	$(CC) $(CFLAGS) -c bitmap.cpp
 clean:
 	rm *.o easyRPGPlayer
 
--- main.cpp
+++ main.cpp
@@ -13,18 +13,19 @@
 
    You should have received a copy of the GNU General Public License
    along with this program.  If not, see <http://www.gnu.org/licenses/>.  */
-   
+
     #include <stdlib.h>
     #include <stdio.h>
     #include "SDL.h"
     #include "tools.h"
     #include "map.h"
+    #include "bitmap.h"
     using namespace std;
-    
+
     SDL_Surface * Screen;
     unsigned long nextTicks = 0, fps = 0, frames = 0;
     char stringBuffer[255];
-    
+
     void CalculateFPS()
     {
         frames++;
@@ -33,45 +34,51 @@
             fps = frames;
             frames = 0;
             nextTicks = SDL_GetTicks() + 1000;
-            
+
             sprintf(stringBuffer, "Map test - FPS %lu", fps);
             SDL_WM_SetCaption (stringBuffer, NULL);
         }
     }
-    
-    int main(int argCounts, char * argStrings[])
-    {       
+
+    int main(int argc, char** argv)
+    {
         // ===[ INITIALIZATION ]================================================
         // Start SDL
         if (SDL_Init (SDL_INIT_VIDEO) < 0) exit(1);
         atexit (SDL_Quit);
-    
+
         const SDL_VideoInfo * videoInfo = SDL_GetVideoInfo();
         unsigned long flags = 0;
-        
+
         if (videoInfo->hw_available) flags |= SDL_HWSURFACE;
         else                         flags |= SDL_SWSURFACE;
         if (videoInfo->blit_hw)      flags |= SDL_HWACCEL;
-        
+
         // Start screen (set to 320x240)
         Screen = SDL_SetVideoMode (320, 240, 16, flags );
         if (Screen == NULL) exit(2);
         SDL_WM_SetCaption ("Map test", NULL);
-        
+
         // ===[ LOADING MAP DATA ]==============================================
         stMap Map;
-        if (argCounts!=2) Map.Load("Map0001.lmu");
-        else              Map.Load(argStrings[1]);       
-        Map.ShowInformation(); 
+        if (argc!=2)
+        	Map.Load("Map0001.lmu");
+        else
+        	Map.Load(argv[1]);
+        Map.ShowInformation();
         Map.Chipset.GenerateFromFile("Basis.png");
-	        
+
         // ===[ ENTRY POINT ]===================================================
         // Main loop
-        bool running = true;        
+        bool running = true;
         SDL_Event event;
         unsigned char * keyData;
         int cX = 0, cY = 0;
-        
+        // TESTING BITMAPS!
+        //Bitmap bmp("Basis.png");
+        //Rect src(387, 32, 88, 64);
+        //Rect dest(0, 0, 64, 64);
+
         while (running)
         {
             // Check for events
@@ -89,24 +96,27 @@
 
             keyData = SDL_GetKeyState(NULL);
             if ( keyData[SDLK_ESCAPE] ) running = false;
-            
+
             if ( keyData[SDLK_LEFT]  ) cX--;
             if ( keyData[SDLK_RIGHT] ) cX++;
             if ( keyData[SDLK_UP]    ) cY--;
             if ( keyData[SDLK_DOWN]  ) cY++;
-            
+
             // Clear screen
             SDL_FillRect(Screen, NULL, 0x0);
-            
+
                 Map.Render(Screen, 0, cX, cY);
                 Map.Render(Screen, 1, cX, cY);
-                
+                /*dest.x = cX;
+                dest.y = cY;
+                bmp.blit(Screen, dest, src);*/
+
             // Flip
             SDL_Flip(Screen);
-            
+
             // Get FPS
             CalculateFPS();
-            
+
             // Delay to ~60 fps
 //            SDL_Delay(1000/60);
         }
