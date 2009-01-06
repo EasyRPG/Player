2b3cf61cc69b97f21e5a85b16ade58e5ec72e4b4
Francisco de la Peña <fran@fran.cr>
2009-01-06 20:54:09 +0000

    FIXED: Terrain tile combination fails render (Closes: #3)

--- chipset.cpp
+++ chipset.cpp
@@ -302,7 +302,7 @@
                             DrawSurface(Destiny, x, y+8, BaseSurface, XTerrain+16, YTerrain+56, 16, 8);
                         } else {
                             DrawSurface(Destiny, x, y, BaseSurface, XTerrain, YTerrain+32, 8, 16);
-                            DrawSurface(Destiny, x+8, y, BaseSurface, XTerrain+32, YTerrain+32, 8, 16);
+                            DrawSurface(Destiny, x+8, y, BaseSurface, XTerrain+40, YTerrain+32, 8, 16);
                         }
                         break;
                     case 0x01:
@@ -368,7 +368,7 @@
             if (Combination&0x01) DrawSurface(Destiny, x, y,     BaseSurface, XTerrain+32, YTerrain, 8, 8);
             if (Combination&0x02) DrawSurface(Destiny, x+8, y,   BaseSurface, XTerrain+40, YTerrain, 8, 8);
             if (Combination&0x04) DrawSurface(Destiny, x+8, y+8, BaseSurface, XTerrain+40, YTerrain+8, 8, 8);
-            if (Combination&0x08) DrawSurface(Destiny, x, y+8,   BaseSurface, XTerrain+32+128, YTerrain+8, 8, 8);
+            if (Combination&0x08) DrawSurface(Destiny, x, y+8,   BaseSurface, XTerrain+32, YTerrain+8, 8, 8);
         }
     }
 
