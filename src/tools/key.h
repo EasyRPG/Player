464b6706cdeb3281103992965dfa4a8b9c3e39e8
Francisco de la Peña <fran@fran.cr>
2009-09-30 01:01:33 +0000

    readded key management

---
+++ src/tools/key.h
@@ -0,0 +1,109 @@
+#ifndef KEY_H
+#define KEY_H
+//LMK=lobo_Mon_keys
+
+//funciones
+#define LMK_ESC  27
+#define LMK_F1   282
+#define LMK_F2   283
+#define LMK_F3   284
+#define LMK_F4   285
+#define LMK_F5   286
+#define LMK_F6   287
+#define LMK_F7   288
+#define LMK_F8   289
+#define LMK_F9   290
+#define LMK_F10  291
+#define LMK_F11  292
+#define LMK_F12  293
+
+//Teclas de desplasamiento
+
+#define LMK_INSERT    277
+#define LMK_INICIO    278
+#define LMK_REGPAG    280
+#define LMK_SUPR      127
+#define LMK_FIN       279
+#define LMK_AVPAG     281
+#define LMK_UP        273
+#define LMK_DOWN      274
+#define LMK_LEFT      275
+#define LMK_RIGHT     276
+
+//Teclado numerico
+#define LMK_div       267
+#define LMK_mult      268
+#define LMK_rest      269
+#define LMK_sum       270
+#define LMK_NUM_INTRO 271
+#define LMK_NUM_PT     266
+#define LMK_NUM_1     257
+#define LMK_NUM_2     258
+#define LMK_NUM_3     259
+#define LMK_NUM_4     260
+#define LMK_NUM_5     261
+#define LMK_NUM_6     262
+#define LMK_NUM_7     263
+#define LMK_NUM_8     264
+#define LMK_NUM_9     265
+#define LMK_NUM_0     256
+
+//Teclado clasico
+
+#define LMK_GRADE     96
+#define LMK_1         49
+#define LMK_2         50
+#define LMK_3         51
+#define LMK_4         52
+#define LMK_5         53
+#define LMK_6         54
+#define LMK_7         55
+#define LMK_8         56
+#define LMK_9         57
+#define LMK_0         48
+#define LMK_QS        45
+#define LMK_QS2       61
+#define LMK_LA         92
+#define LMK_BACK      8
+
+#define LMK_TAB       9
+#define LMK_Q         113
+#define LMK_W         119
+#define LMK_E         101
+#define LMK_R         114
+#define LMK_T         116
+#define LMK_Y         121
+#define LMK_U         117
+#define LMK_I         105
+#define LMK_O         111
+#define LMK_P         112
+#define LMK_CA         91
+#define LMK_CE         93
+#define LMK_INTRO     13
+
+#define LMK_A         97
+#define LMK_S         115
+#define LMK_D         100
+#define LMK_F         102
+#define LMK_G         103
+#define LMK_H         104
+#define LMK_J         106
+#define LMK_K         107
+#define LMK_L         108
+#define LMK_NF         59
+#define LMK_LC         39
+
+#define LMK_MQ         60
+#define LMK_Z         122
+#define LMK_X         120
+#define LMK_C         99
+#define LMK_V         118
+#define LMK_B         98
+#define LMK_N         110
+#define LMK_M         109
+#define LMK_CM         44
+#define LMK_PT         46
+#define LMK__         47
+bool Key_press_and_realsed( unsigned char key );
+
+#endif
