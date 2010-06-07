d2d26600b7bd7afdb045fd7c72f392031c1a8c8d
Alejandro Marzini <vgvgvgf@gmail.com>
2010-06-07 18:30:37 +0000

    Added Spriteset_Map(spriteset_map.h was already in previous commit by error). Added Scene_Map.

---
+++ src/scene_map.h
@@ -0,0 +1,51 @@
+//////////////////////////////////////////////////////////////////////////////////
+/// This file is part of EasyRPG Player.
+/// 
+/// EasyRPG Player is free software: you can redistribute it and/or modify
+/// it under the terms of the GNU General Public License as published by
+/// the Free Software Foundation, either version 3 of the License, or
+/// (at your option) any later version.
+/// 
+/// EasyRPG Player is distributed in the hope that it will be useful,
+/// but WITHOUT ANY WARRANTY; without even the implied warranty of
+/// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+/// GNU General Public License for more details.
+/// 
+/// You should have received a copy of the GNU General Public License
+/// along with EasyRPG Player.  If not, see <http://www.gnu.org/licenses/>.
+//////////////////////////////////////////////////////////////////////////////////
+
+#ifndef _SCENE_MAP_H_
+#define _SCENE_MAP_H_
+
+////////////////////////////////////////////////////////////
+/// Headers
+////////////////////////////////////////////////////////////
+#include "scene.h"
+#include "spriteset.h"
+#include "window_message.h"
+
+////////////////////////////////////////////////////////////
+/// Scene Map class
+////////////////////////////////////////////////////////////
+class Scene_Map: public Scene {
+public:
+    Scene_Map();
+    ~Scene_Map();
+
+    void MainFunction();
+    void Update();
+    void CallBattle();
+	void CallShop();
+	void CallName();
+	void CallMenu();
+	void CallSave();
+	void CallDebug();
+	void TransferPlayer();
+    
+	Spriteset* spriteset;
+    Window_Message* message_window;
+};
+
+#endif
+
---
+++ src/spriteset_map.cpp
@@ -0,0 +1,74 @@
+//////////////////////////////////////////////////////////////////////////////////
+/// This file is part of EasyRPG Player.
+/// 
+/// EasyRPG Player is free software: you can redistribute it and/or modify
+/// it under the terms of the GNU General Public License as published by
+/// the Free Software Foundation, either version 3 of the License, or
+/// (at your option) any later version.
+/// 
+/// EasyRPG Player is distributed in the hope that it will be useful,
+/// but WITHOUT ANY WARRANTY; without even the implied warranty of
+/// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+/// GNU General Public License for more details.
+/// 
+/// You should have received a copy of the GNU General Public License
+/// along with EasyRPG Player.  If not, see <http://www.gnu.org/licenses/>.
+//////////////////////////////////////////////////////////////////////////////////
+
+////////////////////////////////////////////////////////////
+/// Headers
+////////////////////////////////////////////////////////////
+#include "spriteset_map.h"
+#include "main_data.h"
+#include "cache.h"
+
+////////////////////////////////////////////////////////////
+/// Constructor
+////////////////////////////////////////////////////////////
+Spriteset_Map::Spriteset_Map() {
+	tilemap = new Tilemap();
+	tilemap->SetChipset(Cache::Chipset(Main_Data::game_map->chipset_name));
+	tilemap->SetMapDataDown(Main_Data::game_map->GetMapDataDown());
+	tilemap->SetMapDataUp(Main_Data::game_map->GetMapDataUp());
+	panorama = new Plane();
+	panorama->SetZ(-1000);
+	fog = new Plane();
+	fog->SetZ(3000);
+	/*for (int i = 0; i < Main_Data::game_map->events.size; i++) {
+	  Sprite_Character* sprite = new Sprite_Character();
+	  character_sprites.push(sprite);
+	}
+	Sprite_Character* player = new Sprite_Character();
+	character_sprites.push(player);
+	weather = new Weather();
+	for (int i = 0; i < 50; i++) {
+		Sprite_Picture* sprite = new Sprite_Picture();
+		picture_sprites.push(sprite);
+	}
+	timer_sprite = new Sprite_Timer();*/
+    Update();
+}
+
+////////////////////////////////////////////////////////////
+/// Destructor
+////////////////////////////////////////////////////////////
+Spriteset_Map::~Spriteset_Map() {
+	delete tilemap;
+    delete panorama;
+    delete fog;
+    /*for (int i = 0; i < character_sprites.size(); i++) {
+		delete character_sprites[i];
+	}
+	for (int i = 0; i < picture_sprites.size(); i++) {
+		delete picture_sprites[i];
+	}
+	delete weather;
+    delete timer_sprite;*/
+}
+
+////////////////////////////////////////////////////////////
+/// Update
+////////////////////////////////////////////////////////////
+void Spriteset_Map::Update() {
+    
+}
