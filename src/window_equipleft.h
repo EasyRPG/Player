39a6a90403d5fc8493193d37403a61b67190916a
Gabriel Kind <gabelk@web.de>
2010-11-30 17:28:52 +0000

    Add stub classes needed by Scene_Equip.

---
+++ src/window_equipleft.h
@@ -0,0 +1,46 @@
+/////////////////////////////////////////////////////////////////////////////
+// This file is part of EasyRPG Player.
+//
+// EasyRPG Player is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// EasyRPG Player is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with EasyRPG Player. If not, see <http://www.gnu.org/licenses/>.
+/////////////////////////////////////////////////////////////////////////////
+
+#ifndef _WINDOW_EQUIPLEFT_H_
+#define _WINDOW_EQUIPLEFT_H_
+
+////////////////////////////////////////////////////////////
+// Headers
+////////////////////////////////////////////////////////////
+#include "window_base.h"
+
+////////////////////////////////////////////////////////////
+/// Window_EquipLeft class
+////////////////////////////////////////////////////////////
+class Window_EquipLeft : public Window_Base {
+
+public:
+	////////////////////////////////////////////////////////
+	/// Constructor.
+	////////////////////////////////////////////////////////
+	Window_EquipLeft();
+
+	////////////////////////////////////////////////////////
+	/// Desstructor.
+	////////////////////////////////////////////////////////
+	~Window_EquipLeft();
+
+private:
+
+};
+
+#endif
---
+++ src/window_equipright.h
@@ -0,0 +1,46 @@
+/////////////////////////////////////////////////////////////////////////////
+// This file is part of EasyRPG Player.
+//
+// EasyRPG Player is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// EasyRPG Player is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with EasyRPG Player. If not, see <http://www.gnu.org/licenses/>.
+/////////////////////////////////////////////////////////////////////////////
+
+#ifndef _WINDOW_EQUIPRIGHT_H_
+#define _WINDOW_EQUIPRIGHT_H_
+
+////////////////////////////////////////////////////////////
+// Headers
+////////////////////////////////////////////////////////////
+#include "window_selectable.h"
+
+////////////////////////////////////////////////////////////
+/// Window_EquipRight class
+////////////////////////////////////////////////////////////
+class Window_EquipRight : public Window_Selectable {
+
+public:
+	////////////////////////////////////////////////////////
+	/// Constructor.
+	////////////////////////////////////////////////////////
+	Window_EquipRight();
+
+	////////////////////////////////////////////////////////
+	/// Desstructor.
+	////////////////////////////////////////////////////////
+	~Window_EquipRight();
+
+private:
+
+};
+
+#endif
