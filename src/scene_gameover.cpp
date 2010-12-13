c75342953b800be0f89d60fd14e4e7946d831265
niltonpd <niltonpd@users.sourceforge.net>
2010-12-13 03:38:37 +0000

    Added Game Over Scene

---
+++ src/scene_gameover.cpp
@@ -0,0 +1,53 @@
+/////////////////////////////////////////////////////////////////////////////
+// This file is part of EasyRPG Player.
+//
+// EasyRPG Player is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// EasyRPG Player is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with EasyRPG Player. If not, see <http://www.gnu.org/licenses/>.
+/////////////////////////////////////////////////////////////////////////////
+
+////////////////////////////////////////////////////////////
+// Headers
+////////////////////////////////////////////////////////////
+#include "scene_gameover.h"
+#include "audio.h"
+#include "bitmap.h"
+#include "cache.h"
+#include "input.h"
+#include "scene_title.h"
+
+////////////////////////////////////////////////////////////
+Scene_Gameover::Scene_Gameover() {
+	type = Scene::Gameover;
+}
+
+////////////////////////////////////////////////////////////
+Scene_Gameover::~Scene_Gameover() {
+	delete background;
+}
+
+////////////////////////////////////////////////////////////
+void Scene_Gameover::Start() {
+	// Load Background Graphic
+	background = new Sprite();
+	background->SetBitmap(Cache::Gameover(Data::system.gameover_name));
+	// Stop Music
+	Audio::BGM_Stop();
+	Audio::BGS_Stop();
+}
+
+////////////////////////////////////////////////////////////
+void Scene_Gameover::Update() {
+	if (Input::IsTriggered(Input::DECISION)) {
+			Scene::instance = new Scene_Title();
+	}
+}
