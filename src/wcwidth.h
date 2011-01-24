24f8243233fad45d617484e18f41e597598f1754
Glynn Clements <glynn@gclements.plus.com>
2011-01-24 00:18:58 +0000

    Add wcwidth() etc functions

---
+++ src/wcwidth.h
@@ -0,0 +1,8 @@
+#ifndef _EASYRPG_WCWIDTH_H_
+#define _EASYRPG_WCWIDTH_H_
+int mk_wcwidth(wchar_t ucs);
+int mk_wcswidth(const wchar_t *pwcs, size_t n);
+int mk_wcwidth_cjk(wchar_t ucs);
+int mk_wcswidth_cjk(const wchar_t *pwcs, size_t n);
+#endif
+
