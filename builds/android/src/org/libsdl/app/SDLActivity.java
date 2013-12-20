ab07ae4ce88b604b013e3ea24d33ae1909a4ff93
Ghabry <gabriel+github@mastergk.de>
2013-12-20 20:02:37 +0100

    Add eclipse files to ignore list

--- builds/android/src/org/libsdl/app/SDLActivity.java
+++ builds/android/src/org/libsdl/app/SDLActivity.java
@@ -43,7 +43,7 @@ public class SDLActivity extends Activity {
     static {
         System.loadLibrary("SDL2");
         //System.loadLibrary("SDL2_image");
-        //System.loadLibrary("SDL2_mixer");
+        System.loadLibrary("SDL2_mixer");
         //System.loadLibrary("SDL2_net");
         //System.loadLibrary("SDL2_ttf");
         System.loadLibrary("main");
