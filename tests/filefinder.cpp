3c290659f9f9f2ccab7af9bcdd756477b677a905
Francisco de la Peña <fran@fran.cr>
2014-03-25 22:00:36 -0600

    Prepare tests for autotools

---
+++ tests/filefinder.cpp
@@ -0,0 +1,35 @@
+#include <cstdlib>
+#include "filefinder.h"
+
+namespace filefinder_test_anon {
+
+	void CheckIsRPG2kProject() {
+		EASYRPG_SHARED_PTR<FileFinder::ProjectTree> const
+			tree = FileFinder::CreateProjectTree(".");
+		assert(FileFinder::IsRPG2kProject(*tree));
+	}
+
+	void CheckIsDirectory() {
+		assert(FileFinder::IsDirectory("."));
+	}
+
+	void CheckEnglishFilename() {
+		assert(!FileFinder::FindImage("Backdrop", "castle").empty());
+	}
+
+}
+
+using namespace filefinder_test_anon;
+
+int main(int, char**) {
+	FileFinder::Init();
+	FileFinder::InitRtpPaths();
+
+	CheckIsDirectory();
+	CheckIsRPG2kProject();
+	CheckEnglishFilename();
+
+	FileFinder::Quit();
+
+	return EXIT_SUCCESS;
+}
