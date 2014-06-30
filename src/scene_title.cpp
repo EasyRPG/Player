3080f3cd8b23db71900fe070e3ff2c5142330043
Francisco de la Peña <fran@fran.cr>
2014-06-30 07:22:36 -0600

    Allow HideTitle without TestPlay

--- src/scene_title.cpp
+++ src/scene_title.cpp
@@ -53,7 +53,7 @@ Scene_Title::Scene_Title() {
 }
 
 void Scene_Title::Start() {
-	if (!Player::battle_test_flag && !(Player::debug_flag && Player::hide_title_flag)) {
+	if (!Player::battle_test_flag && !Player::hide_title_flag) {
 		CreateTitleGraphic();
 		PlayTitleMusic();
 	}
@@ -74,7 +74,7 @@ void Scene_Title::Continue() {
 void Scene_Title::TransitionIn() {
 	if (!Player::battle_test_flag) {
 		Graphics::Transition(Graphics::TransitionErase, 1, true);
-		if (!(Player::debug_flag && Player::hide_title_flag)) {
+		if (!Player::hide_title_flag) {
 			Graphics::Transition(Graphics::TransitionFadeIn, 32);
 		} else {
 			DisplayUi->SetBackcolor(Cache::system_info.bg_color);
@@ -147,7 +147,7 @@ void Scene_Title::CreateCommandWindow() {
 	options.push_back(Data::terms.exit_game);
 
 	command_window.reset(new Window_Command(options));
-	if (!(Player::debug_flag && Player::hide_title_flag)) {
+	if (!Player::hide_title_flag) {
 		command_window->SetX(SCREEN_TARGET_WIDTH / 2 - command_window->GetWidth() / 2);
 		command_window->SetY(SCREEN_TARGET_HEIGHT / 15 * 13.25 - command_window->GetHeight());
 	} else {
@@ -163,7 +163,7 @@ void Scene_Title::CreateCommandWindow() {
 	}
 
 	// Set the number of frames for the opening animation to last
-	if (!(Player::debug_flag && Player::hide_title_flag)) {
+	if (!Player::hide_title_flag) {
 		command_window->SetOpenAnimation(8);
 	}
 
