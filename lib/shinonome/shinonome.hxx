bb669178afab4911210171affb6e4a9c299b38d4
rohkea <rabina@insiberia.net>
2014-11-01 19:50:56 +0300

    +8 missing Trad. Chinese chars (哎喂拋烤扭髒呦摔) = 100 chars

--- lib/shinonome/shinonome.hxx
+++ lib/shinonome/shinonome.hxx
@@ -9,7 +9,7 @@ struct ShinonomeGlyph {
 	uint16_t data[12];
 };
 
-extern ShinonomeGlyph const SHINONOME_GOTHIC[11152];
+extern ShinonomeGlyph const SHINONOME_GOTHIC[11160];
 extern ShinonomeGlyph const SHINONOME_MINCHO[488];
 
 #endif // _INC_SHINONOME_HXX_
