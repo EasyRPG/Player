EasyRPG/Player

commit 82cd9114b644fd1e5b4ed1118c697e54319ec5b7

Eliminate most uses of BitmapScreen

src/window.cpp
@@ -19,7 +19,6 @@
 #include <math.h>
 #include "system.h"
 #include "graphics.h"
-#include "bitmap_screen.h"
 #include "player.h"
 #include "rect.h"
 #include "util_macro.h"
@@ -55,16 +54,13 @@ Window::Window():
 
 	Graphics::RegisterDrawable(this);
 
-	windowskin_screen = BitmapScreen::Create();
-	contents_screen = BitmapScreen::Create();
-
-	background = BitmapScreen::Create();
-	frame_down = BitmapScreen::Create();
-	frame_up = BitmapScreen::Create();
-	frame_left = BitmapScreen::Create();
-	frame_right = BitmapScreen::Create();
-	cursor1 = BitmapScreen::Create();
-	cursor2 = BitmapScreen::Create();
+	background = BitmapRef();
+	frame_down = BitmapRef();
+	frame_up = BitmapRef();
+	frame_left = BitmapRef();
+	frame_right = BitmapRef();
+	cursor1 = BitmapRef();
+	cursor2 = BitmapRef();
 }
 
 Window::~Window() {
@@ -98,6 +94,7 @@ void Window::Draw() {
 	if (width <= 0 || height <= 0) return;
 	if (x < -width || x > DisplayUi->GetWidth() || y < -height || y > DisplayUi->GetHeight()) return;
 
+	BitmapRef dst = DisplayUi->GetDisplaySurface();
 
 	if (windowskin) {
 		if (width > 4 && height > 4 && (back_opacity * opacity / 255 > 0)) {
@@ -108,9 +105,9 @@ void Window::Draw() {
 
 				Rect src_rect(0, height / 2 - ianimation_count, width, ianimation_count * 2);
 
-				background->BlitScreen(x, y + src_rect.y, src_rect);
+				dst->Blit(x, y + src_rect.y, *background, src_rect, back_opacity * opacity / 255);
 			} else {
-				background->BlitScreen(x, y);
+				dst->Blit(x, y, *background, background->GetRect(), back_opacity * opacity / 255);
 			}
 		}
 
@@ -123,20 +120,20 @@ void Window::Draw() {
 				if (ianimation_count > 8) {
 					Rect src_rect(0, height / 2 - ianimation_count, 8, ianimation_count * 2 - 16);
 
-					frame_left->BlitScreen(x, y + 8 + src_rect.y, src_rect);
-					frame_right->BlitScreen(x + width - 8, y + 8 + src_rect.y, src_rect);
+					dst->Blit(x, y + 8 + src_rect.y, *frame_left, src_rect, opacity);
+					dst->Blit(x + width - 8, y + 8 + src_rect.y, *frame_right, src_rect, opacity);
 
-					frame_up->BlitScreen(x, y + height / 2 - ianimation_count);
-					frame_down->BlitScreen(x, y + height / 2 + ianimation_count - 8);
+					dst->Blit(x, y + height / 2 - ianimation_count, *frame_up, frame_up->GetRect(), opacity);
+					dst->Blit(x, y + height / 2 + ianimation_count - 8, *frame_up, frame_down->GetRect(), opacity);
 				} else {
-					frame_up->BlitScreen(x, y + height / 2 - ianimation_count, Rect(0, 0, width, ianimation_count));
-					frame_down->BlitScreen(x, y + height / 2, Rect(0, 8 - ianimation_count, width, ianimation_count));
+					dst->Blit(x, y + height / 2 - ianimation_count, *frame_up, Rect(0, 0, width, ianimation_count), opacity);
+					dst->Blit(x, y + height / 2 , *frame_down, Rect(0, 8 - ianimation_count, width, ianimation_count), opacity);
 				}
 			} else {
-				frame_up->BlitScreen(x, y);
-				frame_down->BlitScreen(x, y + height - 8);
-				frame_left->BlitScreen(x, y + 8);
-				frame_right->BlitScreen(x + width - 8, y + 8);
+				dst->Blit(x, y, *frame_up, frame_up->GetRect(), opacity);
+				dst->Blit(x, y + height - 8, *frame_down, frame_down->GetRect(), opacity);
+				dst->Blit(x, y + 8, *frame_left, frame_left->GetRect(), opacity);
+				dst->Blit(x + width - 8, y + 8, *frame_right, frame_right->GetRect(), opacity);
 			}
 		}
 
@@ -151,9 +148,9 @@ void Window::Draw() {
 			);
 
 			if (cursor_frame <= 10)
-				cursor1->BlitScreen(x + cursor_rect.x + border_x, y + cursor_rect.y + border_y, src_rect);
+				dst->Blit(x + cursor_rect.x + border_x, y + cursor_rect.y + border_y, *cursor1, src_rect, 255);
 			else
-				cursor2->BlitScreen(x + cursor_rect.x + border_x, y + cursor_rect.y + border_y, src_rect);
+				dst->Blit(x + cursor_rect.x + border_x, y + cursor_rect.y + border_y, *cursor2, src_rect, 255);
 		}
 	}
 
@@ -165,26 +162,25 @@ void Window::Draw() {
 						  min(width - 2 * border_x, width - 2 * border_x + ox),
 						  min(height - 2 * border_y, height - 2 * border_y + oy));
 
-			contents_screen->SetOpacityEffect(contents_opacity);
-
-			contents_screen->BlitScreen(max(x + border_x, x + border_x - ox),
-										max(y + border_y, y + border_y - oy), src_rect);
+			dst->Blit(max(x + border_x, x + border_x - ox),
+					  max(y + border_y, y + border_y - oy),
+					  *contents, src_rect, contents_opacity);
 		}
 	}
 
 	if (pause && pause_frame > 16 && animation_frames <= 0) {
 		Rect src_rect(40, 16, 16, 8);
-		windowskin_screen->BlitScreen(x + width / 2 - 8, y + height - 8, src_rect);
+		dst->Blit(x + width / 2 - 8, y + height - 8, *windowskin, src_rect, 255);
 	}
 
 	if (up_arrow) {
 		Rect src_rect(40, 8, 16, 8);
-		windowskin_screen->BlitScreen(x + width / 2 - 8, y, src_rect);
+		dst->Blit(x + width / 2 - 8, y, *windowskin, src_rect, 255);
 	}
 
 	if (down_arrow) {
 		Rect src_rect(40, 16, 16, 8);
-		windowskin_screen->BlitScreen(x + width / 2 - 8, y + height - 8, src_rect);
+		dst->Blit(x + width / 2 - 8, y + height - 8, *windowskin, src_rect, 255);
 	}
 
 	if (animation_frames > 0) {
@@ -209,7 +205,7 @@ void Window::RefreshBackground() {
 		bitmap->TiledBlit(Rect(0, 0, 16, 16), *windowskin, bitmap->GetRect(), 255);
 	}
 
-	background->SetBitmap(bitmap);
+	background = bitmap;
 }
 
 void Window::RefreshFrame() {
@@ -248,8 +244,8 @@ void Window::RefreshFrame() {
 	// Lower right corner
 	down_bitmap->Blit(width - 8, 0, *windowskin, Rect(64 - 8, 32 - 8, 8, 8), 255);
 
-	frame_up->SetBitmap(up_bitmap);
-	frame_down->SetBitmap(down_bitmap);
+	frame_up = up_bitmap;
+	frame_down = down_bitmap;
 
 	if (height > 16) {
 		BitmapRef left_bitmap = Bitmap::Create(8, height - 16);
@@ -271,11 +267,11 @@ void Window::RefreshFrame() {
 		dst_rect.Set(0, 0, 8, height - 16);
 		right_bitmap->TiledBlit(0, 8, src_rect, *windowskin, dst_rect, 255);
 
-		frame_left->SetBitmap(left_bitmap);
-		frame_right->SetBitmap(right_bitmap);
+		frame_left = left_bitmap;
+		frame_right = right_bitmap;
 	} else {
-		frame_left->SetBitmap(BitmapRef());
-		frame_right->SetBitmap(BitmapRef());
+		frame_left = BitmapRef();
+		frame_right = BitmapRef();
 	}
 }
 
@@ -337,8 +333,8 @@ void Window::RefreshCursor() {
 	cursor1_bitmap->TiledBlit(8, 8, Rect(64 + 8, 8, 16, 16), *windowskin, dst_rect, 255);
 	cursor2_bitmap->TiledBlit(8, 8, Rect(96 + 8, 8, 16, 16), *windowskin, dst_rect, 255);
 
-	cursor1->SetBitmap(cursor1_bitmap);
-	cursor2->SetBitmap(cursor2_bitmap);
+	cursor1 = cursor1_bitmap;
+	cursor2 = cursor2_bitmap;
 }
 
 void Window::Update() {
@@ -360,7 +356,6 @@ void Window::SetWindowskin(BitmapRef const& nwindowskin) {
 	frame_needs_refresh = true;
 	cursor_needs_refresh = true;
 	windowskin = nwindowskin;
-	windowskin_screen->SetBitmap(windowskin);
 }
 
 BitmapRef Window::GetContents() const {
@@ -368,7 +363,6 @@ BitmapRef Window::GetContents() const {
 }
 void Window::SetContents(BitmapRef const& ncontents) {
 	contents = ncontents;
-	contents_screen->SetBitmap(contents);
 }
 
 bool Window::GetStretch() const {
@@ -499,13 +493,6 @@ int Window::GetOpacity() const {
 }
 void Window::SetOpacity(int nopacity) {
 	opacity = nopacity;
-
-	background->SetOpacityEffect(back_opacity * opacity / 255);
-
-	frame_up->SetOpacityEffect(opacity);
-	frame_down->SetOpacityEffect(opacity);
-	frame_left->SetOpacityEffect(opacity);
-	frame_right->SetOpacityEffect(opacity);
 }
 
 int Window::GetBackOpacity() const {
@@ -513,8 +500,6 @@ int Window::GetBackOpacity() const {
 }
 void Window::SetBackOpacity(int nback_opacity) {
 	back_opacity = nback_opacity;
-
-	background->SetOpacityEffect(back_opacity * opacity / 255);
 }
 
 int Window::GetContentsOpacity() const {

src/background.cpp
@@ -23,7 +23,6 @@
 #include "graphics.h"
 #include "cache.h"
 #include "background.h"
-#include "bitmap_screen.h"
 #include "bitmap.h"
 
 Background::Background(const std::string& name) :
@@ -33,7 +32,7 @@ Background::Background(const std::string& name) :
 
 	Graphics::RegisterDrawable(this);
 
-	bg_screen = BitmapScreen::Create(Cache::Backdrop(name));
+	bg_bitmap = Cache::Backdrop(name);
 }
 
 Background::Background(int terrain_id) :
@@ -46,16 +45,16 @@ Background::Background(int terrain_id) :
 	const RPG::Terrain& terrain = Data::terrains[terrain_id - 1];
 
 	if (terrain.background_type == 0) {
-		bg_screen = BitmapScreen::Create(Cache::Backdrop(terrain.background_name));
+		bg_bitmap = Cache::Backdrop(terrain.background_name);
 		return;
 	}
 
-	bg_screen = BitmapScreen::Create(Cache::Frame(terrain.background_a_name));
+	bg_bitmap = Cache::Frame(terrain.background_a_name);
 	bg_hscroll = terrain.background_a_scrollh ? terrain.background_a_scrollh_speed : 0;
 	bg_vscroll = terrain.background_a_scrollv ? terrain.background_a_scrollv_speed : 0;
 
 	if (terrain.background_b) {
-		fg_screen = BitmapScreen::Create(Cache::Frame(terrain.background_b_name));
+		fg_bitmap = Cache::Frame(terrain.background_b_name);
 		fg_hscroll = terrain.background_b_scrollh ? terrain.background_b_scrollh_speed : 0;
 		fg_vscroll = terrain.background_b_scrollv ? terrain.background_b_scrollv_speed : 0;
 	}
@@ -96,13 +95,11 @@ void Background::Draw() {
 	if (!visible)
 		return;
 
-	if (bg_screen)
-		bg_screen->BlitScreenTiled(bg_screen->GetBitmap()->GetRect(),
-								   DisplayUi->GetDisplaySurface()->GetRect(),
-								   Scale(bg_x), Scale(bg_y));
+	BitmapRef dst = DisplayUi->GetDisplaySurface();
 
-	if (fg_screen)
-		fg_screen->BlitScreenTiled(bg_screen->GetBitmap()->GetRect(),
-								   DisplayUi->GetDisplaySurface()->GetRect(),
-								   Scale(fg_x), Scale(fg_y));
+	if (bg_bitmap)
+		dst->TiledBlit(-Scale(bg_x), -Scale(bg_y), bg_bitmap->GetRect(), *bg_bitmap, dst->GetRect(), 255);
+
+	if (fg_bitmap)
+		dst->TiledBlit(-Scale(fg_x), -Scale(fg_y), fg_bitmap->GetRect(), *fg_bitmap, dst->GetRect(), 255);
 }

src/background.h
@@ -44,12 +44,12 @@ private:
 
 	bool visible;
 
-	BitmapScreenRef bg_screen;
+	BitmapRef bg_bitmap;
 	int bg_hscroll;
 	int bg_vscroll;
 	int bg_x;
 	int bg_y;
-	BitmapScreenRef fg_screen;
+	BitmapRef fg_bitmap;
 	int fg_hscroll;
 	int fg_vscroll;
 	int fg_x;

src/battle_animation.cpp
@@ -22,7 +22,7 @@
 #include "filefinder.h"
 #include "cache.h"
 #include "battle_animation.h"
-#include "bitmap_screen.h"
+#include "baseui.h"
 
 BattleAnimation::BattleAnimation(int x, int y, const RPG::Animation* animation) :
 	x(x), y(y), animation(animation), frame(0)
@@ -43,7 +43,7 @@ BattleAnimation::BattleAnimation(int x, int y, const RPG::Animation* animation) 
 		return;
 	}
 
-	screen = BitmapScreen::Create(graphic);
+	screen = graphic;
 
 	Graphics::RegisterDrawable(this);
 }
@@ -78,12 +78,16 @@ void BattleAnimation::Draw() {
 		int sy = cell.cell_id / 5;
 		int size = large ? 128 : 96;
 		int zoomed = size * cell.zoom / 100;
-		screen->SetSrcRect(Rect(sx * size, sy * size, size, size));
-		screen->SetZoomXEffect(cell.zoom / 100.0);
-		screen->SetZoomYEffect(cell.zoom / 100.0);
-		screen->SetToneEffect(Tone(cell.tone_red, cell.tone_green, cell.tone_blue, cell.tone_gray));
-		screen->SetOpacityEffect(255 * (100 - cell.transparency) / 100);
-		screen->BlitScreen(x + cell.x - zoomed / 2, y + cell.y - zoomed / 2);
+		Rect src_rect(sx * size, sy * size, size, size);
+		Tone tone(cell.tone_red, cell.tone_green, cell.tone_blue, cell.tone_gray);
+		int opacity = 255 * (100 - cell.transparency) / 100;
+		double zoom = cell.zoom / 100.0;
+		DisplayUi->GetDisplaySurface()->EffectsBlit(
+			x + cell.x - zoomed / 2, y + cell.y - zoomed / 2,
+			*screen, src_rect, 
+			opacity, 0, 0, tone,
+			zoom, zoom, 0.0,
+			0, 0.0);
 	}
 }
 

src/battle_animation.h
@@ -44,7 +44,7 @@ protected:
 	const RPG::Animation* animation;
 	int frame;
 	bool large;
-	BitmapScreenRef screen;
+	BitmapRef screen;
 };
 
 #endif

src/graphics.cpp
@@ -23,7 +23,6 @@
 
 #include "graphics.h"
 #include "bitmap.h"
-#include "bitmap_screen.h"
 #include "cache.h"
 #include "baseui.h"
 #include "drawable.h"
@@ -48,10 +47,10 @@ namespace Graphics {
 
 	void UpdateTransition();
 
-	BitmapScreenRef frozen_screen;
-	BitmapScreenRef black_screen;
-	BitmapScreenRef screen1;
-	BitmapScreenRef screen2;
+	BitmapRef frozen_screen;
+	BitmapRef black_screen;
+	BitmapRef screen1;
+	BitmapRef screen2;
 	bool frozen;
 	TransitionType transition_type;
 	int transition_duration;
@@ -87,11 +86,9 @@ void Graphics::Init() {
 	framecount = 0;
 	fps_mode = 0;
 	timer_wait = 0;
-	frozen_screen = BitmapScreen::Create();
+	frozen_screen = BitmapRef();
 
-	black_screen = BitmapScreen::Create();
-	BitmapRef black_bitmap = Bitmap::Create(DisplayUi->GetWidth(), DisplayUi->GetHeight(), Color(0,0,0,255));
-	black_screen->SetBitmap(black_bitmap);
+	black_screen = Bitmap::Create(DisplayUi->GetWidth(), DisplayUi->GetHeight(), Color(0,0,0,255));
 
 	frozen = false;
 	drawable_creation = 0;
@@ -317,7 +314,7 @@ BitmapRef Graphics::SnapToBitmap() {
 }
 
 void Graphics::Freeze() {
-	frozen_screen->SetBitmap(SnapToBitmap());
+	frozen_screen = SnapToBitmap();
 	frozen = true;
 }
 
@@ -360,7 +357,7 @@ void Graphics::Transition(TransitionType type, int duration, bool erase) {
 		}
 	}
 
-	if (!erase) frozen_screen->SetBitmap(BitmapRef());
+	if (!erase) frozen_screen = BitmapRef();
 
 	frozen = false;
 	screen_erased = erase;
@@ -372,6 +369,7 @@ void Graphics::Transition(TransitionType type, int duration, bool erase) {
 
 void Graphics::UpdateTransition() {
 	// FIXME: Comments. Pleeeease. screen1, screen2?
+	BitmapRef dst = DisplayUi->GetDisplaySurface();
 	int w = DisplayUi->GetWidth();
 	int h = DisplayUi->GetHeight();
 
@@ -400,9 +398,8 @@ void Graphics::UpdateTransition() {
 	switch (transition_type) {
 	case TransitionFadeIn:
 	case TransitionFadeOut:
-		screen1->BlitScreen(0, 0);
-		screen2->SetOpacityEffect(255 * percentage / 100);
-		screen2->BlitScreen(0, 0);
+		dst->Blit(0, 0, *screen1, screen1->GetRect(), 255);
+		dst->Blit(0, 0, *screen2, screen2->GetRect(), 255 * percentage / 100);
 		break;
 	case TransitionRandomBlocks:
 		break;
@@ -412,105 +409,105 @@ void Graphics::UpdateTransition() {
 		break;
 	case TransitionBlindOpen:
 		for (int i = 0; i < h / 8; i++) {
-			screen1->BlitScreen(0, i * 8, Rect(0, i * 8, w, 8 - 8 * percentage / 100));
-			screen2->BlitScreen(0, i * 8 + 8 - 8 * percentage / 100, Rect(0, i * 8 + 8 - 8 * percentage / 100, w, 8 * percentage / 100));
+			dst->Blit(0, i * 8, *screen1, Rect(0, i * 8, w, 8 - 8 * percentage / 100), 255);
+			dst->Blit(0, i * 8 + 8 - 8 * percentage / 100, *screen2, Rect(0, i * 8 + 8 - 8 * percentage / 100, w, 8 * percentage / 100), 255 * percentage / 100);
 		}
 		break;
 	case TransitionBlindClose:
 		for (int i = 0; i < h / 8; i++) {
-			screen1->BlitScreen(0, i * 8 + 8 * percentage / 100, Rect(0, i * 8 + 8 * percentage / 100, w, 8 - 8 * percentage / 100));
-			screen2->BlitScreen(0, i * 8, Rect(0, i * 8, w, 8 * percentage / 100));
+			dst->Blit(0, i * 8 + 8 * percentage / 100, *screen1, Rect(0, i * 8 + 8 * percentage / 100, w, 8 - 8 * percentage / 100), 255);
+			dst->Blit(0, i * 8, *screen2, Rect(0, i * 8, w, 8 * percentage / 100), 255);
 		}
 		break;
 	case TransitionVerticalStripesIn:
 	case TransitionVerticalStripesOut:
 		for (int i = 0; i < h / 6 + 1 - h / 6 * percentage / 100; i++) {
-			screen1->BlitScreen(0, i * 6 + 3, Rect(0, i * 6 + 3, w, 3));
-			screen1->BlitScreen(0, h - i * 6, Rect(0, h - i * 6, w, 3));
+			dst->Blit(0, i * 6 + 3, *screen1, Rect(0, i * 6 + 3, w, 3), 255);
+			dst->Blit(0, h - i * 6, *screen1, Rect(0, h - i * 6, w, 3), 255);
 		}
 		for (int i = 0; i < h / 6 * percentage / 100; i++) {
-			screen2->BlitScreen(0, i * 6, Rect(0, i * 6, w, 3));
-			screen2->BlitScreen(0, h - 3 - i * 6, Rect(0, h - 3 - i * 6, w, 3));
+			dst->Blit(0, i * 6, *screen2, Rect(0, i * 6, w, 3), 255);
+			dst->Blit(0, h - 3 - i * 6, *screen2, Rect(0, h - 3 - i * 6, w, 3), 255);
 		}
 		break;
 	case TransitionHorizontalStripesIn:
 	case TransitionHorizontalStripesOut:
 		for (int i = 0; i < w / 8 + 1 - w / 8 * percentage / 100; i++) {
-			screen1->BlitScreen(i * 8 + 4, 0, Rect(i * 8 + 4, 0, 4, h));
-			screen1->BlitScreen(w  - i * 8, 0, Rect(w - i * 8, 0, 4, h));
+			dst->Blit(i * 8 + 4, 0, *screen1, Rect(i * 8 + 4, 0, 4, h), 255);
+			dst->Blit(w - i * 8, 0, *screen1, Rect(w - i * 8, 0, 4, h), 255);
 		}
 		for (int i = 0; i < w / 8 * percentage / 100; i++) {
-			screen2->BlitScreen(i * 8, 0, Rect(i * 8, 0, 4, h));
-			screen2->BlitScreen(w - 4 - i * 8, 0, Rect(w - 4 - i * 8, 0, 4, h));
+			dst->Blit(i * 8, 0, *screen2, Rect(i * 8, 0, 4, h), 255);
+			dst->Blit(w - 4 - i * 8, 0, *screen2, Rect(w - 4 - i * 8, 0, 4, h), 255);
 		}
 		break;
 	case TransitionBorderToCenterIn:
 	case TransitionBorderToCenterOut:
-		screen2->BlitScreen(0, 0);
-		screen1->BlitScreen((w / 2) * percentage / 100, (h / 2) * percentage / 100, Rect((w / 2) * percentage / 100, (h / 2) * percentage / 100, w - w * percentage / 100, h - h * percentage / 100));
+		dst->Blit(0,  0, *screen2, screen2->GetRect(), 255);
+		dst->Blit((w / 2) * percentage / 100, (h / 2) * percentage / 100, *screen1, Rect((w / 2) * percentage / 100, (h / 2) * percentage / 100, w - w * percentage / 100, h - h * percentage / 100), 255);
 		break;
 	case TransitionCenterToBorderIn:
 	case TransitionCenterToBorderOut:
-		screen1->BlitScreen(0, 0);
-		screen2->BlitScreen(w / 2 - (w / 2) * percentage / 100, h / 2 - (h / 2) * percentage / 100, Rect(w / 2 - (w / 2) * percentage / 100, h / 2 - (h / 2) * percentage / 100, w * percentage / 100, h * percentage / 100));
+		dst->Blit(0,  0, *screen1, screen1->GetRect(), 255);
+		dst->Blit(w / 2 - (w / 2) * percentage / 100, h / 2 - (h / 2) * percentage / 100, *screen2, Rect(w / 2 - (w / 2) * percentage / 100, h / 2 - (h / 2) * percentage / 100, w * percentage / 100, h * percentage / 100), 255);
 		break;
 	case TransitionScrollUpIn:
 	case TransitionScrollUpOut:
-		screen1->BlitScreen(0, -h * percentage / 100);
-		screen2->BlitScreen(0, h - h * percentage / 100);
+		dst->Blit(0,  -h * percentage / 100, *screen1, screen1->GetRect(), 255);
+		dst->Blit(0,  h - h * percentage / 100, *screen2, screen2->GetRect(), 255);
 		break;
 	case TransitionScrollDownIn:
 	case TransitionScrollDownOut:
-		screen1->BlitScreen(0, h * percentage / 100);
-		screen2->BlitScreen(0, -h + h * percentage / 100);
+		dst->Blit(0,  h * percentage / 100, *screen1, screen1->GetRect(), 255);
+		dst->Blit(0,  -h + h * percentage / 100, *screen2, screen2->GetRect(), 255);
 		break;
 	case TransitionScrollLeftIn:
 	case TransitionScrollLeftOut:
-		screen1->BlitScreen(-w * percentage / 100, 0);
-		screen2->BlitScreen(w - w * percentage / 100, 0);
+		dst->Blit(-w * percentage / 100,  0, *screen1, screen1->GetRect(), 255);
+		dst->Blit(w - w * percentage / 100,  0, *screen2, screen2->GetRect(), 255);
 		break;
 	case TransitionScrollRightIn:
 	case TransitionScrollRightOut:
-		screen1->BlitScreen(w * percentage / 100, 0);
-		screen2->BlitScreen(-w + w * percentage / 100, 0);
+		dst->Blit(w * percentage / 100,  0, *screen1, screen1->GetRect(), 255);
+		dst->Blit(-w + w * percentage / 100,  0, *screen2, screen2->GetRect(), 255);
 		break;
 	case TransitionVerticalCombine:
-		screen1->BlitScreen(0, (h / 2) * percentage / 100, Rect(0, (h / 2) * percentage / 100, w, h - h * percentage / 100));
-		screen2->BlitScreen(0, -h / 2 + (h / 2) * percentage / 100, Rect(0, 0, w, h / 2));
-		screen2->BlitScreen(0, h - (h / 2) * percentage / 100, Rect(0, h / 2, w, h / 2));
+		dst->Blit(0, (h / 2) * percentage / 100, *screen1, Rect(0, (h / 2) * percentage / 100, w, h - h * percentage / 100), 255);
+		dst->Blit(0, -h / 2 + (h / 2) * percentage / 100, *screen2, Rect(0, 0, w, h / 2), 255);
+		dst->Blit(0, h - (h / 2) * percentage / 100, *screen2, Rect(0, h / 2, w, h / 2), 255);
 		break;
 	case TransitionVerticalDivision:
-		screen1->BlitScreen(0, -(h / 2) * percentage / 100, Rect(0, 0, w, h / 2));
-		screen1->BlitScreen(0, h / 2 + (h / 2) * percentage / 100, Rect(0, h / 2, w, h / 2));
-		screen2->BlitScreen(0, h / 2 - (h / 2) * percentage / 100, Rect(0, h / 2 - (h / 2) * percentage / 100, w, h * percentage / 100));
+		dst->Blit(0, -(h / 2) * percentage / 100, *screen1, Rect(0, 0, w, h / 2), 255);
+		dst->Blit(0, h / 2 + (h / 2) * percentage / 100, *screen1, Rect(0, h / 2, w, h / 2), 255);
+		dst->Blit(0, h / 2 - (h / 2) * percentage / 100, *screen2, Rect(0, h / 2 - (h / 2) * percentage / 100, w, h * percentage / 100), 255);
 		break;
 	case TransitionHorizontalCombine:
-		screen1->BlitScreen((w / 2) * percentage / 100, 0, Rect((w / 2) * percentage / 100, 0, w - w * percentage / 100, h));
-		screen2->BlitScreen(- w / 2 + (w / 2) * percentage / 100, 0, Rect(0, 0, w / 2, h));
-		screen2->BlitScreen(w - (w / 2) * percentage / 100, 0, Rect(w / 2, 0, w / 2, h));
+		dst->Blit((w / 2) * percentage / 100, 0, *screen1, Rect((w / 2) * percentage / 100, 0, w - w * percentage / 100, h), 255);
+		dst->Blit(- w / 2 + (w / 2) * percentage / 100, 0, *screen2, Rect(0, 0, w / 2, h), 255);
+		dst->Blit(w - (w / 2) * percentage / 100, 0, *screen2, Rect(w / 2, 0, w / 2, h), 255);
 		break;
 	case TransitionHorizontalDivision:
-		screen1->BlitScreen(-(w / 2) * percentage / 100, 0, Rect(0, 0, w / 2, h));
-		screen1->BlitScreen(w / 2 + (w / 2) * percentage / 100, 0, Rect(w / 2, 0, w / 2, h));
-		screen2->BlitScreen(w / 2 - (w / 2) * percentage / 100, 0, Rect(w / 2 - (w / 2) * percentage / 100, 0, w * percentage / 100, h));
+		dst->Blit(-(w / 2) * percentage / 100, 0, *screen1, Rect(0, 0, w / 2, h), 255);
+		dst->Blit(w / 2 + (w / 2) * percentage / 100, 0, *screen1, Rect(w / 2, 0, w / 2, h), 255);
+		dst->Blit(w / 2 - (w / 2) * percentage / 100, 0, *screen2, Rect(w / 2 - (w / 2) * percentage / 100, 0, w * percentage / 100, h), 255);
 		break;
 	case TransitionCrossCombine:
-		screen1->BlitScreen((w / 2) * percentage / 100, 0, Rect((w / 2) * percentage / 100, 0, w - w * percentage / 100, (h / 2) * percentage / 100));
-		screen1->BlitScreen((w / 2) * percentage / 100, h - (h / 2) * percentage / 100, Rect((w / 2) * percentage / 100, h - (h / 2) * percentage / 100, w - w * percentage / 100, (h / 2) * percentage / 100));
-		screen1->BlitScreen(0, (h / 2) * percentage / 100, Rect(0, (h / 2) * percentage / 100, w, h - h * percentage / 100));
-		screen2->BlitScreen(- w / 2 + (w / 2) * percentage / 100, -h / 2 + (h / 2) * percentage / 100, Rect(0, 0, w / 2, h / 2));
-		screen2->BlitScreen(w - (w / 2) * percentage / 100, -h / 2 + (h / 2) * percentage / 100, Rect(w / 2, 0, w / 2, h / 2));
-		screen2->BlitScreen(w - (w / 2) * percentage / 100, h - (h / 2) * percentage / 100, Rect(w / 2, h / 2, w / 2, h / 2));
-		screen2->BlitScreen(- w / 2 + (w / 2) * percentage / 100, h - (h / 2) * percentage / 100, Rect(0, h / 2, w / 2, h / 2));
+		dst->Blit((w / 2) * percentage / 100, 0, *screen1, Rect((w / 2) * percentage / 100, 0, w - w * percentage / 100, (h / 2) * percentage / 100), 255);
+		dst->Blit((w / 2) * percentage / 100, h - (h / 2) * percentage / 100, *screen1, Rect((w / 2) * percentage / 100, h - (h / 2) * percentage / 100, w - w * percentage / 100, (h / 2) * percentage / 100), 255);
+		dst->Blit(0, (h / 2) * percentage / 100, *screen1, Rect(0, (h / 2) * percentage / 100, w, h - h * percentage / 100), 255);
+		dst->Blit(- w / 2 + (w / 2) * percentage / 100, -h / 2 + (h / 2) * percentage / 100, *screen2, Rect(0, 0, w / 2, h / 2), 255);
+		dst->Blit(w - (w / 2) * percentage / 100, -h / 2 + (h / 2) * percentage / 100, *screen2, Rect(w / 2, 0, w / 2, h / 2), 255);
+		dst->Blit(w - (w / 2) * percentage / 100, h - (h / 2) * percentage / 100, *screen2, Rect(w / 2, h / 2, w / 2, h / 2), 255);
+		dst->Blit(- w / 2 + (w / 2) * percentage / 100, h - (h / 2) * percentage / 100, *screen2, Rect(0, h / 2, w / 2, h / 2), 255);
 		break;
 	case TransitionCrossDivision:
-		screen1->BlitScreen(-(w / 2) * percentage / 100, -(h / 2) * percentage / 100, Rect(0, 0, w / 2, h / 2));
-		screen1->BlitScreen(w / 2 + (w / 2) * percentage / 100, -(h / 2) * percentage / 100, Rect(w / 2, 0, w / 2, h / 2));
-		screen1->BlitScreen(w / 2 + (w / 2) * percentage / 100, h / 2 + (h / 2) * percentage / 100, Rect(w / 2, h / 2, w / 2, h / 2));
-		screen1->BlitScreen(-(w / 2) * percentage / 100, h / 2 + (h / 2) * percentage / 100, Rect(0, h / 2, w / 2, h / 2));
-		screen2->BlitScreen(w / 2 - (w / 2) * percentage / 100, 0, Rect(w / 2 - (w / 2) * percentage / 100, 0, w * percentage / 100, h / 2 - (h / 2) * percentage / 100));
-		screen2->BlitScreen(w / 2 - (w / 2) * percentage / 100, h / 2 + (h / 2) * percentage / 100, Rect(w / 2 - (w / 2) * percentage / 100, h / 2 + (h / 2) * percentage / 100, w * percentage / 100, h / 2 + (h / 2) * percentage / 100));
-		screen2->BlitScreen(0, h / 2 - (h / 2) * percentage / 100, Rect(0, h / 2 - (h / 2) * percentage / 100, w, h * percentage / 100));
+		dst->Blit(-(w / 2) * percentage / 100, -(h / 2) * percentage / 100, *screen1, Rect(0, 0, w / 2, h / 2), 255);
+		dst->Blit(w / 2 + (w / 2) * percentage / 100, -(h / 2) * percentage / 100, *screen1, Rect(w / 2, 0, w / 2, h / 2), 255);
+		dst->Blit(w / 2 + (w / 2) * percentage / 100, h / 2 + (h / 2) * percentage / 100, *screen1, Rect(w / 2, h / 2, w / 2, h / 2), 255);
+		dst->Blit(-(w / 2) * percentage / 100, h / 2 + (h / 2) * percentage / 100, *screen1, Rect(0, h / 2, w / 2, h / 2), 255);
+		dst->Blit(w / 2 - (w / 2) * percentage / 100, 0, *screen2, Rect(w / 2 - (w / 2) * percentage / 100, 0, w * percentage / 100, h / 2 - (h / 2) * percentage / 100), 255);
+		dst->Blit(w / 2 - (w / 2) * percentage / 100, h / 2 + (h / 2) * percentage / 100, *screen2, Rect(w / 2 - (w / 2) * percentage / 100, h / 2 + (h / 2) * percentage / 100, w * percentage / 100, h / 2 + (h / 2) * percentage / 100), 255);
+		dst->Blit(0, h / 2 - (h / 2) * percentage / 100, *screen2, Rect(0, h / 2 - (h / 2) * percentage / 100, w, h * percentage / 100), 255);
 		break;
 	case TransitionZoomIn:
 		break;

src/message_overlay.cpp
@@ -33,11 +33,9 @@ MessageOverlay::MessageOverlay() :
 	counter(0),
 	show_all(false) {
 	
-	bitmap_screen = BitmapScreen::Create();
 	black = Bitmap::Create(DisplayUi->GetWidth(), text_height, Color());
 
 	bitmap = Bitmap::Create(DisplayUi->GetWidth(), text_height * message_max, true);
-	bitmap_screen->SetBitmap(bitmap);
 
 	Graphics::RegisterDrawable(this);
 }
@@ -67,7 +65,7 @@ void MessageOverlay::Draw() {
 		}
 	} else {
 		if (!messages.empty()) {
-			bitmap_screen->BlitScreen(ox, oy);
+			DisplayUi->GetDisplaySurface()->Blit(ox, oy, *bitmap, bitmap->GetRect(), 255);
 		}
 	}
 
@@ -86,7 +84,7 @@ void MessageOverlay::Draw() {
 		}
 	}
 
-	bitmap_screen->BlitScreen(ox, oy);
+	DisplayUi->GetDisplaySurface()->Blit(ox, oy, *bitmap, bitmap->GetRect(), 255);
 
 	dirty = false;
 }

src/message_overlay.h
@@ -20,7 +20,6 @@
 
 #include <deque>
 #include "bitmap.h"
-#include "bitmap_screen.h"
 #include "color.h"
 #include "drawable.h"
 #include "tone.h"
@@ -60,7 +59,6 @@ private:
 
 	BitmapRef bitmap;
 	BitmapRef black;
-	BitmapScreenRef bitmap_screen;
 
 	int z;
 	int ox;

commit e17cf94e7a1c279d5c7085c87d078be6fb2346b2

Fix #544, use the bottom frame to blit from (copy-and-paste error).
This fixes a regression introduced in 82cd9114b644
"Eliminate most uses of BitmapScreen".

src/window.cpp
@@ -124,7 +124,7 @@ void Window::Draw() {
 					dst->Blit(x + width - 8, y + 8 + src_rect.y, *frame_right, src_rect, opacity);
 
 					dst->Blit(x, y + height / 2 - ianimation_count, *frame_up, frame_up->GetRect(), opacity);
-					dst->Blit(x, y + height / 2 + ianimation_count - 8, *frame_up, frame_down->GetRect(), opacity);
+					dst->Blit(x, y + height / 2 + ianimation_count - 8, *frame_down, frame_down->GetRect(), opacity);
 				} else {
 					dst->Blit(x, y + height / 2 - ianimation_count, *frame_up, Rect(0, 0, width, ianimation_count), opacity);
 					dst->Blit(x, y + height / 2 , *frame_down, Rect(0, 8 - ianimation_count, width, ianimation_count), opacity);