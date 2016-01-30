EasyRPG/Player

commit 63d03e0338c26ce912967755aa5c5e2c0dc7c5df

Tweak message waiting with parallel interpreters

src/game_interpreter.cpp
@@ -157,8 +157,12 @@ void Game_Interpreter::Update() {
 			}
 		}
 
-		if (Game_Message::message_waiting && (main_flag || Game_Message::owner_id == event_id)) {
-			break;
+		if (main_flag) {
+			if (Game_Message::message_waiting)
+				break;
+		} else {
+			if (Game_Message::visible && Game_Message::owner_id == event_id)
+				break;
 		}
 
 		if (wait_count > 0) {

commit fec790d86a0116b4fbd8e560c158c533863ea797

Fix a bug introduced in 63d03e03 which breaks UiD.

src/game_interpreter.cpp
@@ -161,7 +161,7 @@ void Game_Interpreter::Update() {
 			if (Game_Message::message_waiting)
 				break;
 		} else {
-			if (Game_Message::visible && Game_Message::owner_id == event_id)
+			if ((Game_Message::visible || Game_Message::message_waiting) && Game_Message::owner_id == event_id)
 				break;
 		}
 