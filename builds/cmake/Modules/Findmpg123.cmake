28748ebcaf2a8b5789de34c4ea5d1627479dfc7c
Francisco de la Peña <fran@fran.cr>
2016-03-29 09:37:35 -0600

    CMake: add libmpg123 support

---
+++ builds/cmake/Modules/Findmpg123.cmake
@@ -0,0 +1,10 @@
+find_path(MPG123_INCLUDE_DIR mpg123.h)
+find_library(MPG123_LIBRARY NAMES mpg123 libmpg123)
+
+include(FindPackageHandleStandardArgs)
+find_package_handle_standard_args(MPG123 DEFAULT_MSG MPG123_INCLUDE_DIR MPG123_LIBRARY)
+
+set(MPG123_INCLUDE_DIRS ${MPG123_INCLUDE_DIR})
+set(MPG123_LIBRARIES ${MPG123_LIBRARY})
+
+mark_as_advanced(MPG123_INCLUDE_DIR MPG123_LIBRARY)
