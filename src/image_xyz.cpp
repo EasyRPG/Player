8d8a49a49503a6636089d832740ef7ffd2d531c2
Carsten Teibes <dev@f4ke.de>
2016-05-27 19:29:19 +0200

    Refactor XYZ reader, fix spacing, use warnings only

--- src/image_xyz.cpp
+++ src/image_xyz.cpp
@@ -26,37 +26,38 @@
 #include "output.h"
 #include "image_xyz.h"
 
-void ImageXYZ::ReadXYZ(const uint8_t* data, unsigned len, bool transparent,
-					int& width, int& height, void*& pixels) {
-	pixels = NULL;
+bool ImageXYZ::ReadXYZ(const uint8_t* data, unsigned len, bool transparent,
+					   int& width, int& height, void*& pixels) {
+	pixels = nullptr;
 
-    if (len < 8) {
-		Output::Error("Not a valid XYZ file.");
-		return;
-    }
+	if (len < 8) {
+		Output::Warning("Not a valid XYZ file.");
+		return false;
+	}
 
-    unsigned short w = data[4] + (data[5] << 8);
-    unsigned short h = data[6] + (data[7] << 8);
-
+	uint16_t w = data[4] + (data[5] << 8);
+	uint16_t h = data[6] + (data[7] << 8);
 	uLongf src_size = len - 8;
-    Bytef* src_buffer = (Bytef*)&data[8];
-    uLongf dst_size = 768 + (w * h);
+	Bytef* src_buffer = (Bytef*)&data[8];
+	uLongf dst_size = 768 + (w * h);
 	std::vector<Bytef> dst_buffer(dst_size);
 
-    int status = uncompress(&dst_buffer.front(), &dst_size, src_buffer, src_size);
+	int status = uncompress(&dst_buffer.front(), &dst_size, src_buffer, src_size);
 	if (status != Z_OK) {
-		Output::Error("Error decompressing XYZ file.");
-		return;
+		Output::Warning("Error decompressing XYZ file.");
+		return false;
 	}
-    const uint8_t (*palette)[3] = (const uint8_t(*)[3]) &dst_buffer.front();
+	const uint8_t (*palette)[3] = (const uint8_t(*)[3]) &dst_buffer.front();
 
-	width = w;
-	height = h;
 	pixels = malloc(w * h * 4);
+	if (!pixels) {
+		Output::Warning("Error allocating XYZ pixel buffer.");
+		return false;
+	}
 
-    uint8_t* dst = (uint8_t*) pixels;
-    const uint8_t* src = (const uint8_t*) &dst_buffer[768];
-    for (int y = 0; y < h; y++) {
+	uint8_t* dst = (uint8_t*) pixels;
+	const uint8_t* src = (const uint8_t*) &dst_buffer[768];
+	for (int y = 0; y < h; y++) {
 		for (int x = 0; x < w; x++) {
 			uint8_t pix = *src++;
 			const uint8_t* color = palette[pix];
@@ -65,22 +66,25 @@ void ImageXYZ::ReadXYZ(const uint8_t* data, unsigned len, bool transparent,
 			*dst++ = color[2];
 			*dst++ = (transparent && pix == 0) ? 0 : 255;
 		}
-    }
+	}
+
+	width = w;
+	height = h;
+	return true;
 }
 
-void ImageXYZ::ReadXYZ(FILE* stream, bool transparent,
-					int& width, int& height, void*& pixels) {
-    fseek(stream, 0, SEEK_END);
-    long size = ftell(stream);
-    fseek(stream, 0, SEEK_SET);
+bool ImageXYZ::ReadXYZ(FILE* stream, bool transparent,
+					   int& width, int& height, void*& pixels) {
+	fseek(stream, 0, SEEK_END);
+	long size = ftell(stream);
+	fseek(stream, 0, SEEK_SET);
 	std::vector<uint8_t> buffer(size);
-    long size_read = fread((void*) &buffer.front(), 1, size, stream);
-    if (size_read != size) {
-        Output::Error("Error reading XYZ file.");
-        return;
-    }
-	ReadXYZ(&buffer.front(), (unsigned) size, transparent, width, height, pixels);
+	long size_read = fread((void*) &buffer.front(), 1, size, stream);
+	if (size_read != size) {
+		Output::Warning("Error reading XYZ file.");
+		return false;
+	}
+	return ReadXYZ(&buffer.front(), (unsigned) size, transparent, width, height, pixels);
 }
-
 
 #endif // SUPPORT_XYZ
