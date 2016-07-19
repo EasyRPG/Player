EasyRPG/Player

commit 81c910a5f9af5a97e9602ed98c4bb3ad1ebde846

Support "half sp cost" flag for equipped items

src/game_actor.cpp
@@ -141,6 +141,21 @@ bool Game_Actor::IsSkillUsable(int skill_id) const {
 	return Game_Battler::IsSkillUsable(skill_id);
 }
 
+int Game_Actor::CalculateSkillCost(int skill_id) const {
+	int start = GetTwoSwordsStyle() ? 1 : 0;
+	int sp_mod = 1;
+
+	for (int i = start; i < 5; ++i) {
+		const RPG::Item* item = GetEquipment(i);
+		if (item && item->half_sp_cost) {
+			sp_mod = 2;
+			break;
+		}
+	}
+	
+	return Game_Battler::CalculateSkillCost(skill_id) / sp_mod;
+}
+
 bool Game_Actor::LearnSkill(int skill_id) {
 	if (skill_id > 0 && !IsSkillLearned(skill_id)) {
 		GetData().skills.push_back((int16_t)skill_id);

src/game_actor.h
@@ -112,6 +112,14 @@ public:
 	bool IsSkillUsable(int skill_id) const override;
 
 	/**
+	 * Calculates the Skill costs including all modifiers.
+	 *
+	 * @param skill_id ID of skill to calculate.
+	 * @return needed skill cost.
+	 */
+	int CalculateSkillCost(int skill_id) const override;
+
+	/**
 	 * Gets the actor ID.
 	 *
 	 * @return Actor ID

src/game_battlealgorithm.cpp
@@ -601,7 +601,7 @@ void Game_BattleAlgorithm::Normal::Apply() {
 
 	source->SetCharged(false);
 	if (source->GetType() == Game_Battler::Type_Ally && static_cast<Game_Actor*>(source)->GetWeaponId() != 0) {
-		source->ChangeSp(-Data::items[static_cast<Game_Actor*>(source)->GetWeaponId() -1].sp_cost);
+		source->ChangeSp(-source->CalculateSkillCost(static_cast<Game_Actor*>(source)->GetWeaponId()));
 	}
 }
 

src/game_battler.h
@@ -336,7 +336,7 @@ public:
 	 * @param skill_id ID of skill to calculate.
 	 * @return needed skill cost.
 	 */
-	int CalculateSkillCost(int skill_id) const;
+	virtual int CalculateSkillCost(int skill_id) const;
 
 	/**
 	 * Sets the battler attack modifier.

src/window_skill.cpp
@@ -76,7 +76,9 @@ void Window_Skill::DrawItem(int index) {
 	int skill_id = data[index];
 
 	if (skill_id > 0) {
-		int costs = Data::skills[skill_id - 1].sp_cost;
+		const Game_Actor* actor = Game_Actors::GetActor(actor_id);
+		int costs = actor->CalculateSkillCost(skill_id);
+
 		bool enabled = CheckEnable(skill_id);
 		int color = !enabled ? Font::ColorDisabled : Font::ColorDefault;
 

commit d468e3062e23b4ce42bb10f68497b81209f2f0b6

The SP cost calculation for weapon usage in RPG2k3 erroneously used the weapon id as the skill id instead of the sp_cost of the weapon which makes weapon use really expensive. This fixes a regression introduced in 81c910a5.

src/game_actor.cpp
@@ -141,7 +141,7 @@ bool Game_Actor::IsSkillUsable(int skill_id) const {
 	return Game_Battler::IsSkillUsable(skill_id);
 }
 
-int Game_Actor::CalculateSkillCost(int skill_id) const {
+int Game_Actor::GetSpCostModifier() const {
 	int start = HasTwoWeapons() ? 1 : 0;
 	int sp_mod = 1;
 
@@ -152,8 +152,12 @@ int Game_Actor::CalculateSkillCost(int skill_id) const {
 			break;
 		}
 	}
-	
-	return Game_Battler::CalculateSkillCost(skill_id) / sp_mod;
+
+	return sp_mod;
+}
+
+int Game_Actor::CalculateSkillCost(int skill_id) const {
+	return Game_Battler::CalculateSkillCost(skill_id) / GetSpCostModifier();
 }
 
 bool Game_Actor::LearnSkill(int skill_id) {

src/game_actor.h
@@ -112,6 +112,13 @@ public:
 	bool IsSkillUsable(int skill_id) const override;
 
 	/**
+	 * Returns the modifier by which skill costs are divided.
+	 *
+	 * @return modifier
+	 */
+	int GetSpCostModifier() const;
+
+	/**
 	 * Calculates the Skill costs including all modifiers.
 	 *
 	 * @param skill_id ID of skill to calculate.

src/game_battlealgorithm.cpp
@@ -638,8 +638,11 @@ void Game_BattleAlgorithm::Normal::Apply() {
 	AlgorithmBase::Apply();
 
 	source->SetCharged(false);
-	if (source->GetType() == Game_Battler::Type_Ally && static_cast<Game_Actor*>(source)->GetWeaponId() != 0) {
-		source->ChangeSp(-source->CalculateSkillCost(static_cast<Game_Actor*>(source)->GetWeaponId()));
+	if (source->GetType() == Game_Battler::Type_Ally) {
+		Game_Actor* src = static_cast<Game_Actor*>(source);
+		if (src->GetWeaponId() != 0) {
+			source->ChangeSp(-Data::items[src->GetWeaponId() - 1].sp_cost / src->GetSpCostModifier());
+		}
 	}
 }
 