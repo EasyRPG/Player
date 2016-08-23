9369f13ddcb537c3d084aa33edca014f737894dd
Dmitry Marakasov <amdmi3@amdmi3.ru>
2016-08-23 18:15:57 +0300

    NULL does not need to be casted, also switch to nullptr
    Fixes clang build error

--- src/font.cpp
+++ src/font.cpp
@@ -145,7 +145,7 @@ BitmapRef ShinonomeFont::Glyph(char32_t code) {
 	assert(glyph);
 	size_t const width = glyph->is_full? FULL_WIDTH : HALF_WIDTH;
 
-	BitmapRef bm = Bitmap::Create(reinterpret_cast<void*>(NULL), width, HEIGHT, 0, DynamicFormat(8,8,0,8,0,8,0,8,0,PF::Alpha));
+	BitmapRef bm = Bitmap::Create(nullptr, width, HEIGHT, 0, DynamicFormat(8,8,0,8,0,8,0,8,0,PF::Alpha));
 	uint8_t* data = reinterpret_cast<uint8_t*>(bm->pixels());
 	int pitch = bm->pitch();
 	for(size_t y_ = 0; y_ < HEIGHT; ++y_)
@@ -194,7 +194,7 @@ BitmapRef FTFont::Glyph(char32_t glyph) {
 	int const width = ft_bitmap.width;
 	int const height = ft_bitmap.rows;
 
-	BitmapRef bm = Bitmap::Create(reinterpret_cast<void*>(NULL), width, height, 0, DynamicFormat(8,8,0,8,0,8,0,8,0,PF::Alpha));
+	BitmapRef bm = Bitmap::Create(nullptr, width, height, 0, DynamicFormat(8,8,0,8,0,8,0,8,0,PF::Alpha));
 	uint8_t* data = reinterpret_cast<uint8_t*>(bm->pixels());
 	int dst_pitch = bm->pitch();
 
