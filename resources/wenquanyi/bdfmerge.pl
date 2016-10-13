bca07275ca64f5c6df7202877b272061e590de41
DBLobster@dev <db.lobster@gmail.com>
2016-10-13 09:09:08 +0800

    Fix: bdfmerge array. gen CJK Basic font.
    ./bdfmerge.pl 0x4E00--0x9FCB wenquanyi_9pt.bdf > wenquanyi_cjk_basic_9pt.bdf
    
    0x4E00--0x9FFF is section of Unicdoe CJK Basic.
    WenQuanYi(wqy) is Unicode 5.2 fonts, so 0x4E00--0x9FCB.

--- resources/wenquanyi/bdfmerge.pl
+++ resources/wenquanyi/bdfmerge.pl
@@ -32,7 +32,7 @@ while(@ARGV>=1&& $ARGV[0]=~/^-{0,1}0[xX]([0-9A-Fa-f]{0,6})/)
 {
 	$startpos = hex($1);
 	$endpos=0;
-	$range=shift(ARGV);
+	$range=shift(@ARGV);
 	if($range=~/--0[xX]([0-9A-Fa-f]{0,6})/)
 	{
 		$endpos=hex($1);
