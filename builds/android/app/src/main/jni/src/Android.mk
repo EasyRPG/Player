b06dd33032f5fe59003f1657967fec429c1e0c24
Carsten Teibes <dev@f4ke.de>
2017-05-27 02:56:01 +0200

    Android: Refactor build files, move deps to deps/Android.mk, cleanup

--- builds/android/app/src/main/jni/src/Android.mk
+++ builds/android/app/src/main/jni/src/Android.mk
@@ -1,29 +1,15 @@
 LOCAL_PATH := $(call my-dir)
 
+EASYRPG_TOOLCHAIN_DIR = $(EASYDEV_ANDROID)/$(TARGET_ARCH_ABI)-toolchain
+
 include $(CLEAR_VARS)
 
 LOCAL_MODULE := main
 
 PLAYER_PATH := ../../../../../../../
 
-ifeq ($(TARGET_ARCH_ABI),x86)
-        EASYRPG_TOOLCHAIN_DIR = $(EASYDEV_ANDROID)/x86-toolchain
-else ifeq ($(TARGET_ARCH_ABI),armeabi)
-        EASYRPG_TOOLCHAIN_DIR = $(EASYDEV_ANDROID)/armeabi-toolchain
-else ifeq ($(TARGET_ARCH_ABI),armeabi-v7a)
-        EASYRPG_TOOLCHAIN_DIR = $(EASYDEV_ANDROID)/armeabi-v7a-toolchain
-else ifeq ($(TARGET_ARCH_ABI),mips)
-        EASYRPG_TOOLCHAIN_DIR = $(EASYDEV_ANDROID)/mips-toolchain
-endif
-
 LOCAL_C_INCLUDES := \
 	$(LOCAL_PATH)/$(PLAYER_PATH)/src \
-	$(EASYRPG_TOOLCHAIN_DIR)/include \
-	$(EASYRPG_TOOLCHAIN_DIR)/include/liblcf \
-	$(EASYRPG_TOOLCHAIN_DIR)/include/pixman-1 \
-	$(EASYRPG_TOOLCHAIN_DIR)/include/freetype2 \
-	$(EASYRPG_TOOLCHAIN_DIR)/include/libpng16 \
-	$(EASYRPG_TOOLCHAIN_DIR)/include/libxmp-lite \
 	$(EASYRPG_TOOLCHAIN_DIR)/include/SDL2
 
 # Add your application source files here...
@@ -33,21 +19,21 @@ LOCAL_SRC_FILES := SDL_android_main.c \
 
 LOCAL_SHARED_LIBRARIES := SDL2
 
-LOCAL_STATIC_LIBRARIES := cpufeatures
+LOCAL_STATIC_LIBRARIES := \
+		SDL2_mixer vorbisfile vorbis ogg \
+		xmp-lite mpg123 speexdsp sndfile \
+		freetype pixman-1 png \
+		lcf expat icui18n icuuc icudata \
+		cpufeatures
 
-LOCAL_LDLIBS := -L$(LOCAL_PATH)/../../obj/local/$(TARGET_ARCH_ABI) \
-		-L$(EASYRPG_TOOLCHAIN_DIR)/lib -lGLESv1_CM -llog -lz \
-		-lSDL2_mixer -lvorbisfile -lvorbis -logg -lxmp-lite \
-		-lmpg123 -lfreetype -lpixman-1 -lpng -lspeexdsp -lsndfile \
-		-llcf -lexpat -licui18n -licuuc -licudata \
-		-lcpufeatures
+LOCAL_LDLIBS := -lGLESv1_CM -llog -lz
 
-LOCAL_CFLAGS := -O2 -Wall -Wextra -fno-rtti -DUSE_SDL \
+LOCAL_CFLAGS := -O2 -Wall -Wextra -DUSE_SDL \
 		-DHAVE_SDL_MIXER -DHAVE_MPG123 -DWANT_FMMIDI=2 \
 		-DHAVE_OGGVORBIS -DHAVE_XMP -DHAVE_LIBSNDFILE \
 		-DHAVE_LIBSPEEXDSP -DSUPPORT_AUDIO
 
-		LOCAL_CPPFLAGS	=	$(LOCAL_C_FLAGS) -std=c++11
+LOCAL_CXXFLAGS := $(LOCAL_C_FLAGS) -std=c++11 -fno-rtti
 
 include $(BUILD_SHARED_LIBRARY)
 
