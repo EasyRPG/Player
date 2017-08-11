EasyRPG/Player

commit 1269f70230575d1d5a3f1e6354270d03c4e2f427

Replace +=, -=, ++, -- operations on and referencing of liblcf data fields.
This is required for the liblcf vNext rewrite and my automatic converter can't handle these special cases.

src/game_map.cpp
@@ -329,12 +329,14 @@ void Game_Map::ReserveInterpreterDeletion(std::shared_ptr<Game_Interpreter> inte
 }
 
 void Game_Map::ScrollRight(int distance) {
-	AddScreenX(map_info.position_x, distance);
+	int x = map_info.position_x;
+	AddScreenX(x, distance);
 	Parallax::Scroll(distance, 0);
 }
 
 void Game_Map::ScrollDown(int distance) {
-	AddScreenY(map_info.position_y, distance);
+	int y = map_info.position_y;
+	AddScreenY(y, distance);
 	Parallax::Scroll(0, distance);
 }
 
@@ -876,7 +878,7 @@ void Game_Map::UpdateEncounterSteps() {
 	int terrain_id = GetTerrainTag(x, y);
 	const RPG::Terrain& terrain = Data::terrains[terrain_id - 1];
 
-	location.encounter_steps -= terrain.encounter_rate;
+	location.encounter_steps = location.encounter_steps - terrain.encounter_rate;
 
 	if (location.encounter_steps <= 0) {
 		ResetEncounterSteps();
@@ -1200,12 +1202,21 @@ void Game_Map::UnlockPan() {
 
 void Game_Map::StartPan(int direction, int distance, int speed, bool wait) {
 	distance *= SCREEN_TILE_WIDTH;
-	switch (direction) {
-		case PanUp:		location.pan_finish_y -= distance;		break;
-		case PanRight:	location.pan_finish_x += distance;		break;
-		case PanDown:	location.pan_finish_y += distance;		break;
-		case PanLeft:	location.pan_finish_x -= distance;		break;
+
+	if (direction == PanUp) {
+		int new_pan = location.pan_finish_y - distance;
+		location.pan_finish_y = new_pan;
+	} else if (direction == PanRight) {
+		int new_pan = location.pan_finish_x + distance;
+		location.pan_finish_x = new_pan;
+	} else if (direction == PanDown) {
+		int new_pan = location.pan_finish_y + distance;
+		location.pan_finish_y = new_pan;
+	} else if (direction == PanLeft) {
+		int new_pan = location.pan_finish_x - distance;
+		location.pan_finish_x = new_pan;
 	}
+
 	pan_speed = speed;
 	pan_wait = wait;
 }
@@ -1225,15 +1236,21 @@ void Game_Map::UpdatePan() {
 	int dx = location.pan_finish_x - location.pan_current_x;
 	int dy = location.pan_finish_y - location.pan_current_y;
 
-	if (dx > 0)
-		location.pan_current_x += std::min(step, dx);
-	else if (dx < 0)
-		location.pan_current_x -= std::min(step, -dx);
+	if (dx > 0) {
+		int pan = location.pan_current_x + std::min(step, dx);
+		location.pan_current_x = pan;
+	} else if (dx < 0) {
+		int pan = location.pan_current_x - std::min(step, -dx);
+		location.pan_current_x = pan;
+	}
 
-	if (dy > 0)
-		location.pan_current_y += std::min(step, dy);
-	else if (dy < 0)
-		location.pan_current_y -= std::min(step, -dy);
+	if (dy > 0) {
+		int pan = location.pan_current_y + std::min(step, dy);
+		location.pan_current_y = pan;
+	} else if (dy < 0) {
+		int pan = location.pan_current_y - std::min(step, -dy);
+		location.pan_current_y = pan;
+	}
 }
 
 bool Game_Map::IsPanActive() {

src/game_actor.cpp
@@ -1030,12 +1030,16 @@ std::string Game_Actor::GetClassName() const {
 }
 
 void Game_Actor::SetBaseMaxHp(int maxhp) {
-	GetData().hp_mod += maxhp - GetBaseMaxHp();
+	int new_hp_mod = GetData().hp_mod + (maxhp - GetBaseMaxHp());
+	GetData().hp_mod = new_hp_mod;
+
 	SetHp(GetData().current_hp);
 }
 
 void Game_Actor::SetBaseMaxSp(int maxsp) {
-	GetData().sp_mod += maxsp - GetBaseMaxSp();
+	int new_sp_mod = GetData().sp_mod + (maxsp - GetBaseMaxSp());
+	GetData().sp_mod = new_sp_mod;
+
 	SetSp(GetData().current_sp);
 }
 
@@ -1068,19 +1072,23 @@ void Game_Actor::SetSp(int sp) {
 }
 
 void Game_Actor::SetBaseAtk(int atk) {
-	GetData().attack_mod += atk - GetBaseAtk();
+	int new_attack_mod = GetData().attack_mod + (atk - GetBaseAtk());
+	GetData().attack_mod = new_attack_mod;
 }
 
 void Game_Actor::SetBaseDef(int def) {
-	GetData().defense_mod += def - GetBaseDef();
+	int new_defense_mod = GetData().defense_mod + (def - GetBaseDef());
+	GetData().defense_mod = new_defense_mod;
 }
 
 void Game_Actor::SetBaseSpi(int spi) {
-	GetData().spirit_mod += spi - GetBaseSpi();
+	int new_spirit_mod = GetData().spirit_mod + (spi - GetBaseSpi());
+	GetData().spirit_mod = new_spirit_mod;
 }
 
 void Game_Actor::SetBaseAgi(int agi) {
-	GetData().agility_mod += agi - GetBaseAgi();
+	int new_agility_mod = GetData().agility_mod + (agi - GetBaseAgi());
+	GetData().agility_mod = new_agility_mod;
 }
 
 int Game_Actor::GetBattleRow() const {

src/game_party.cpp
@@ -107,12 +107,12 @@ int Game_Party::GetItemCount(int item_id, bool get_equipped) {
 }
 
 void Game_Party::GainGold(int n) {
-	data.gold += n;
+	data.gold = data.gold + n;
 	data.gold = std::min(std::max(data.gold, 0), 999999);
 }
 
 void Game_Party::LoseGold(int n) {
-	data.gold -= n;
+	data.gold = data.gold - n;
 	data.gold = std::min(std::max(data.gold, 0), 999999);
 }
 
@@ -470,7 +470,7 @@ void Game_Party::StopTimer(int which) {
 void Game_Party::UpdateTimers() {
 	bool battle = Game_Temp::battle_running;
 	if (data.timer1_active && (data.timer1_battle || !battle) && data.timer1_secs > 0) {
-		data.timer1_secs--;
+		data.timer1_secs = data.timer1_secs - 1;
 		if (data.timer1_secs % DEFAULT_FPS == 0) {
 			Game_Map::SetNeedRefresh(Game_Map::Refresh_Map);
 		}
@@ -479,7 +479,7 @@ void Game_Party::UpdateTimers() {
 		}
 	}
 	if (data.timer2_active && (data.timer2_battle || !battle) && data.timer2_secs > 0) {
-		data.timer2_secs--;
+		data.timer2_secs = data.timer2_secs - 1;
 		if (data.timer2_secs % DEFAULT_FPS == 0) {
 			Game_Map::SetNeedRefresh(Game_Map::Refresh_Map);
 		}

src/game_picture.cpp
@@ -187,14 +187,14 @@ void Game_Picture::Update() {
 		if (old_map_x != Game_Map::GetDisplayX()) {
 			double mx = (old_map_x - Game_Map::GetDisplayX()) / (double)TILE_SIZE;
 
-			data.finish_x += mx;
-			data.current_x += mx;
+			data.finish_x = data.finish_x + mx;
+			data.current_x = data.current_x + mx;
 		}
 		if (old_map_y != Game_Map::GetDisplayY()) {
 			double my = (old_map_y - Game_Map::GetDisplayY()) / (double)TILE_SIZE;
 
-			data.finish_y += my;
-			data.current_y += my;
+			data.finish_y = data.finish_y + my;
+			data.current_y = data.current_y + my;
 		}
 
 		old_map_x = Game_Map::GetDisplayX();
@@ -204,30 +204,30 @@ void Game_Picture::Update() {
 	if (data.time_left == 0) {
 		SyncCurrentToFinish();
 	} else {
-		auto interpolate = [=](double& current, double finish) {
+		auto interpolate = [=](double current, double finish) {
 			double d = data.time_left;
-			current = (current * (d - 1) + finish) / d;
+			return (current * (d - 1) + finish) / d;
 		};
 
-		interpolate(data.current_x, data.finish_x);
-		interpolate(data.current_y, data.finish_y);
-		interpolate(data.current_red, data.finish_red);
-		interpolate(data.current_green, data.finish_green);
-		interpolate(data.current_blue, data.finish_blue);
-		interpolate(data.current_sat, data.finish_sat);
-		interpolate(data.current_magnify, data.finish_magnify);
-		interpolate(data.current_top_trans, data.finish_top_trans);
-		interpolate(data.current_bot_trans, data.finish_bot_trans);
+		data.current_x = interpolate(data.current_x, data.finish_x);
+		data.current_y = interpolate(data.current_y, data.finish_y);
+		data.current_red = interpolate(data.current_red, data.finish_red);
+		data.current_green = interpolate(data.current_green, data.finish_green);
+		data.current_blue = interpolate(data.current_blue, data.finish_blue);
+		data.current_sat = interpolate(data.current_sat, data.finish_sat);
+		data.current_magnify = interpolate(data.current_magnify, data.finish_magnify);
+		data.current_top_trans = interpolate(data.current_top_trans, data.finish_top_trans);
+		data.current_bot_trans = interpolate(data.current_bot_trans, data.finish_bot_trans);
 		if (data.effect_mode != 0) {
-			interpolate(data.current_effect, data.finish_effect);
+			data.current_effect = interpolate(data.current_effect, data.finish_effect);
 		}
 
-		data.time_left--;
+		data.time_left = data.time_left - 1;
 	}
 
 	// Update rotation
 	if (data.current_rotation >= 256.0) {
-		data.current_rotation -= 256.0;
+		data.current_rotation = data.current_rotation - 256.0;
 	}
 	bool is_rotating_but_stopping =
 		data.effect_mode == 0 && (
@@ -238,7 +238,7 @@ void Game_Picture::Update() {
 		data.effect_mode == 1 ||
 		is_rotating_but_stopping;
 	if (is_rotating) {
-		data.current_rotation += data.current_effect;
+		data.current_rotation = data.current_rotation + data.current_effect;
 		if (is_rotating_but_stopping && data.current_rotation >= 256.0) {
 			data.current_rotation = 0.0;
 		}
@@ -246,7 +246,7 @@ void Game_Picture::Update() {
 
 	// Update waver phase
 	if (data.effect_mode == 2) {
-		data.current_waver += 10;
+		data.current_waver = data.current_waver + 10;
 	}
 
 	UpdateSprite();

src/game_screen.cpp
@@ -232,12 +232,12 @@ void Game_Screen::Update() {
 		data.tint_current_green = interpolate(data.tint_time_left, data.tint_current_green, data.tint_finish_green);
 		data.tint_current_blue = interpolate(data.tint_time_left, data.tint_current_blue, data.tint_finish_blue);
 		data.tint_current_sat = interpolate(data.tint_time_left, data.tint_current_sat, data.tint_finish_sat);
-		data.tint_time_left--;
+		data.tint_time_left = data.tint_time_left - 1;
 	}
 
 	if (data.flash_time_left > 0) {
-		data.flash_current_level -= data.flash_current_level / data.flash_time_left;
-		data.flash_time_left--;
+		data.flash_current_level = data.flash_current_level - (data.flash_current_level / data.flash_time_left);
+		data.flash_time_left = data.flash_time_left - 1;
 		if (data.flash_time_left <= 0)
 			data.flash_time_left = data.flash_continuous ? flash_period : 0;
 	}
@@ -247,14 +247,14 @@ void Game_Screen::Update() {
 		if (data.shake_time_left <= 1 && data.shake_position * (data.shake_position + delta) < 0)
 			data.shake_position = 0;
 		else
-			data.shake_position += (int)delta;
+			data.shake_position = data.shake_position + (int)delta;
 		if (data.shake_position > data.shake_strength * 2)
 			shake_direction = -1;
 		if (data.shake_position < -data.shake_strength * 2)
 			shake_direction = 1;
 
 		if (data.shake_time_left > 0)
-			data.shake_time_left--;
+			data.shake_time_left = data.shake_time_left - 1;
 	}
 
 	for (const auto& picture : pictures) {

src/game_vehicle.cpp
@@ -405,11 +405,11 @@ void Game_Vehicle::Update() {
 
 	if (type == Airship) {
 		if (IsAscending()) {
-			data.remaining_ascent -= 8;
+			data.remaining_ascent = data.remaining_ascent - 8;
 			if (!IsAscending())
 				walk_animation = true;
 		} else if (IsDescending()) {
-			data.remaining_descent -= 8;
+			data.remaining_descent = data.remaining_descent - 8;
 			if (!IsDescending()) {
 				if (CanLand()) {
 					SetLayer(RPG::EventPage::Layers_same);

src/scene_save.cpp
@@ -87,7 +87,7 @@ void Scene_Save::Action(int index) {
 	Main_Data::game_data.title = title;
 
 	Main_Data::game_data.system.save_slot = index + 1;
-	Main_Data::game_data.system.save_count += 1;
+	Main_Data::game_data.system.save_count = Main_Data::game_data.system.save_count + 1;
 
 	Game_Map::PrepareSave();
 

commit 9af89a5c56f77ad18d011d684f3bc3c0432b4600

Fix screen scrolling which I broke in 1269f70.
Fix #1241

src/game_map.cpp
@@ -331,12 +331,14 @@ void Game_Map::ReserveInterpreterDeletion(std::shared_ptr<Game_Interpreter> inte
 void Game_Map::ScrollRight(int distance) {
 	int x = map_info.position_x;
 	AddScreenX(x, distance);
+	map_info.position_x = x;
 	Parallax::Scroll(distance, 0);
 }
 
 void Game_Map::ScrollDown(int distance) {
 	int y = map_info.position_y;
 	AddScreenY(y, distance);
+	map_info.position_y = y;
 	Parallax::Scroll(0, distance);
 }
 