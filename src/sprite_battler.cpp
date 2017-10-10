EasyRPG/Player

commit 9088f4f678f9ceeee331b519eb30ca6155157084

Remove redundant SetX,Y,Z calls in Sprite_Battler.
Fixes a regression introduced in #1062

src/sprite_battler.cpp
@@ -144,10 +144,6 @@ void Sprite_Battler::Update() {
 			}
 		}
 	}
-
-	SetX(battler->GetDisplayX());
-	SetY(battler->GetDisplayY());
-	SetZ(battler->GetBattleY());
 }
 
 void Sprite_Battler::SetAnimationState(int state, LoopState loop) {

commit 61918b8d1711e92b0a2edc555a6b6dfb17a6fc18

Restore shaking battlers This fixes a regression in commit 9088f4f678f9, battlers stay fixed when the screen is shaking.

src/sprite_battler.cpp
@@ -144,6 +144,9 @@ void Sprite_Battler::Update() {
 			}
 		}
 	}
+
+	// needed for screen shaking
+	SetX(battler->GetDisplayX());
 }
 
 void Sprite_Battler::SetAnimationState(int state, LoopState loop) {