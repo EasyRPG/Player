EasyRPG/Player

commit 5fba5fb8c80a1402adfaeedf18740e877da25601

Add placeholders to undamaged and critical messages

src/game_battlealgorithm.cpp
@@ -226,6 +226,45 @@ std::string Game_BattleAlgorithm::AlgorithmBase::GetHPMPRecoveredMessage(int val
 	}
 }
 
+
+std::string Game_BattleAlgorithm::AlgorithmBase::GetUndamagedMessage() const {
+	bool target_is_ally = (GetTarget()->GetType() ==
+			Game_Battler::Type_Ally);
+	const std::string& message = target_is_ally ?
+		Data::terms.actor_undamaged :
+		Data::terms.enemy_undamaged;
+
+	if (Player::IsRPG2kE()) {
+		return Utils::ReplacePlaceholders(
+			message,
+			{'S'},
+			{GetTarget()->GetName()}
+		);
+	}
+	else {
+		return GetTarget()->GetName() + message;
+	}
+}
+
+std::string Game_BattleAlgorithm::AlgorithmBase::GetCriticalHitMessage() const {
+	bool target_is_ally = (GetTarget()->GetType() ==
+			Game_Battler::Type_Ally);
+	const std::string& message = target_is_ally ?
+		Data::terms.actor_critical :
+		Data::terms.enemy_critical;
+
+	if (Player::IsRPG2kE()) {
+		return Utils::ReplacePlaceholders(
+			message,
+			{'S'},
+			{GetTarget()->GetName()}
+		);
+	}
+	else {
+		return GetTarget()->GetName() + message;
+	}
+}
+
 void Game_BattleAlgorithm::AlgorithmBase::GetResultMessages(std::vector<std::string>& out) const {
 	if (current_target == targets.end()) {
 		return;
@@ -250,15 +289,11 @@ void Game_BattleAlgorithm::AlgorithmBase::GetResultMessages(std::vector<std::str
 
 			ss << GetTarget()->GetName();
 			if (critical_hit) {
-				out.push_back(target_is_ally ?
-					Data::terms.actor_critical :
-					Data::terms.enemy_critical);
+				out.push_back(GetCriticalHitMessage());
 			}
 
 			if (GetAffectedHp() == 0) {
-				ss << (target_is_ally ?
-					Data::terms.actor_undamaged :
-					Data::terms.enemy_undamaged);
+				out.push_back(GetUndamagedMessage());
 			}
 			else {
 				if (absorb) {
@@ -286,8 +321,8 @@ void Game_BattleAlgorithm::AlgorithmBase::GetResultMessages(std::vector<std::str
 						Data::terms.actor_damaged :
 						Data::terms.enemy_damaged);
 				}
-			}
 			out.push_back(ss.str());
+			}
 		}
 	}
 

src/game_battlealgorithm.h
@@ -306,6 +306,8 @@ protected:
 
 	std::string GetDodgeMessage() const;
 	std::string GetHPMPRecoveredMessage(int value, const std::string& points) const;
+	std::string GetUndamagedMessage() const;
+	std::string GetCriticalHitMessage() const;
 
 	float GetAttributeMultiplier(const std::vector<bool>& attributes_set) const;
 

commit 529fe26e4ae5ba6379b7fc98f8a65d16b800069d

Battle 2k: Don't insert target name into critical hit message if placeholder is not used
Fix #1291 (regression introduced in commit 5fba5fb)

src/game_battlealgorithm.cpp
@@ -271,7 +271,7 @@ std::string Game_BattleAlgorithm::AlgorithmBase::GetCriticalHitMessage() const {
 		);
 	}
 	else {
-		return GetTarget()->GetName() + message;
+		return message;
 	}
 }
 