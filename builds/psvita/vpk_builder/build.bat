0a5cbbcc5778b7ae3627d57e9b6346f4213f5d02
Rinnegatamante <rinnegatamante@gmail.com>
2018-03-23 21:54:33 +0100

    [PSVITA] Applied extended memory mode to vpk builder too.

--- builds/psvita/vpk_builder/build.bat
+++ builds/psvita/vpk_builder/build.bat
@@ -3,7 +3,7 @@
 set PATH=%CD%;%PATH%
 set /p id="Insert vpk title ID [4 characters, only uppercase] (Example: AAAA): "
 set /p title="Insert Game Title: "
-vita-mksfoex -s TITLE_ID=%id%00001 "%title%" build\sce_sys\param.sfo
+vita-mksfoex -s TITLE_ID=%id%00001 -d ATTRIBUTE2=12 "%title%" build\sce_sys\param.sfo
 pngquant assets\bg.png -o build\sce_sys\livearea\contents\bg.png
 pngquant assets\icon0.png -o build\sce_sys\icon0.png
 pngquant assets\startup.png -o build\sce_sys\livearea\contents\startup.png
