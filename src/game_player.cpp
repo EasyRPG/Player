EasyRPG/Player

commit b7f307e04f80f847fda8ab3a3b3280d0f7ac85fe

When entering in boat/ship, not synchronize vehicle until the hero is inside
Problem: In RPG_RT, the character moves to the vehicle, then disappears and then the vehicle synchronizes with the hero's movement. In EasyRPG, the vehicle synchronizes before the character has moved towards it, so it teleports and moves even then.

Solution: This happens because the function SyncWithPlayer, which only Game_Player should execute once it's boarded inside, it's used every turn in Game_Vehicle needlessly, rendering the other call useless. It's taken away.

src/game_vehicle.cpp
@@ -412,7 +412,6 @@ bool Game_Vehicle::CanLand() const {
 void Game_Vehicle::Update() {
 	Game_Character::Update();
 	Game_Character::UpdateSprite();
-	SyncWithPlayer();
 
 	if (type == Airship) {
 		if (IsAscending()) {

commit cfb8d9e0f8462061b97a5694016f01b606ae0a9f

Fix a regression introduced in b7f307e which resulted in a glitchy airship sprite
Closes #1458

src/game_player.cpp
@@ -435,9 +435,6 @@ void Game_Player::Update() {
 	Game_Character::UpdateSprite();
 	UpdateScroll();
 
-	if (location.aboard)
-		GetVehicle()->SyncWithPlayer();
-
 	if (IsMoving() || was_blocked) return;
 
 	if (last_moving && location.boarding) {

src/game_vehicle.cpp
@@ -413,6 +413,10 @@ void Game_Vehicle::Update() {
 	Game_Character::Update();
 	Game_Character::UpdateSprite();
 
+	if (!Main_Data::game_player->IsBoardingOrUnboarding()) {
+		SyncWithPlayer();
+	}
+
 	if (type == Airship) {
 		if (IsAscending()) {
 			data.remaining_ascent = data.remaining_ascent - 8;