48099b0743aa2017a1e483d48e2bf05d260e14f4
Matthew Fioravante <fmatthew5876@gmail.com>
2018-11-18 11:43:40 -0500

    Implement continuous flash (rm2k3)

--- src/game_screen.cpp
+++ src/game_screen.cpp
@@ -143,11 +143,13 @@ void Game_Screen::FlashBegin(int r, int g, int b, int s, int tenths) {
 	FlashOnce(r, g, b, s, tenths);
 
 	flash_period = data.flash_time_left;
+	data.flash_continuous = true;
 }
 
 void Game_Screen::FlashEnd() {
 	data.flash_time_left = 0;
 	flash_period = 0;
+	data.flash_continuous = false;
 }
 
 void Game_Screen::ShakeOnce(int power, int speed, int tenths) {
@@ -240,8 +242,13 @@ void Game_Screen::Update() {
 	if (data.flash_time_left > 0) {
 		data.flash_current_level = data.flash_current_level - (data.flash_current_level / data.flash_time_left);
 		data.flash_time_left = data.flash_time_left - 1;
-		if (data.flash_time_left <= 0)
-			data.flash_time_left = data.flash_continuous ? flash_period : 0;
+		if (data.flash_time_left <= 0) {
+			data.flash_time_left = 0;
+			if (data.flash_continuous) {
+				data.flash_time_left = flash_period;
+				data.flash_current_level = flash_sat;
+			}
+		}
 	}
 
 	if (data.shake_continuous || data.shake_time_left > 0 || data.shake_position != 0) {
