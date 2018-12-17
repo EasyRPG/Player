d8101604064f45c25a13087a20e58a50303df1a4
Matthew Fioravante <fmatthew5876@gmail.com>
2018-12-16 20:10:09 -0500

    Switch from gtest to doctest

--- tests/wordwrap.cpp
+++ tests/wordwrap.cpp
@@ -1,6 +1,8 @@
-#include <gtest/gtest.h>
 #include "game_message.h"
 #include "options.h"
+
+#define DOCTEST_CONFIG_IMPLEMENT_WITH_MAIN
+#include "doctest.h"
 
 constexpr int limit_2k = SCREEN_TARGET_WIDTH - 20;
 
@@ -11,85 +13,85 @@ static std::vector<std::string> WordWrap(const std::string& line, int limit = li
 }
 
 
-TEST(WordWrapTest, Empty) {
+TEST_CASE("Empty") {
 	auto lines = WordWrap("");
-	ASSERT_EQ(lines.size(), 0);
+	REQUIRE_EQ(lines.size(), 0);
 
 	lines = WordWrap("", 0);
-	ASSERT_EQ(lines.size(), 0);
+	REQUIRE_EQ(lines.size(), 0);
 
 	lines = WordWrap("a", 0);
-	ASSERT_EQ(lines.size(), 1);
-	ASSERT_EQ(lines[0], "a");
+	REQUIRE_EQ(lines.size(), 1);
+	REQUIRE_EQ(lines[0], "a");
 }
 
-TEST(WordWrapTest, 1char) {
+TEST_CASE("1char") {
 	auto lines = WordWrap("a");
-	ASSERT_EQ(lines.size(), 1);
-	ASSERT_EQ(lines[0], "a");
+	REQUIRE_EQ(lines.size(), 1);
+	REQUIRE_EQ(lines[0], "a");
 }
 
-TEST(WordWrapTest, normal) {
+TEST_CASE("normal") {
 	std::string line = "Skeleton Attacks!";
 	auto lines = WordWrap(line);
-	ASSERT_EQ(lines.size(), 1);
-	ASSERT_EQ(lines[0], line);
+	REQUIRE_EQ(lines.size(), 1);
+	REQUIRE_EQ(lines[0], line);
 
 	line = "Alex takes 300 damage!";
 	lines = WordWrap(line);
-	ASSERT_EQ(lines.size(), 1);
-	ASSERT_EQ(lines[0], line);
+	REQUIRE_EQ(lines.size(), 1);
+	REQUIRE_EQ(lines[0], line);
 }
 
-TEST(WordWrapTest, toolong) {
+TEST_CASE("toolong") {
 	std::string base;
 	for (int i = 0; i < 6; ++i) {
 		base += "Skeleton";
 	}
 	auto lines = WordWrap(base + "XYZ");
-	ASSERT_EQ(lines.size(), 1);
-	ASSERT_EQ(lines[0], base + "XYZ");
+	REQUIRE_EQ(lines.size(), 1);
+	REQUIRE_EQ(lines[0], base + "XYZ");
 
 	lines = WordWrap(base + " XYZ");
-	ASSERT_EQ(lines.size(), 2);
-	ASSERT_EQ(lines[0], base);
-	ASSERT_EQ(lines[1], "XYZ");
+	REQUIRE_EQ(lines.size(), 2);
+	REQUIRE_EQ(lines[0], base);
+	REQUIRE_EQ(lines[1], "XYZ");
 }
 
-TEST(WordWrapTest, limits) {
+TEST_CASE("limits") {
 	const std::string nstr = "0123456789";
 	const std::string limit = nstr + nstr + nstr + nstr + nstr;
 
 	auto lines = WordWrap(limit);
-	ASSERT_EQ(lines.size(), 1);
-	ASSERT_EQ(lines[0], limit);
+	REQUIRE_EQ(lines.size(), 1);
+	REQUIRE_EQ(lines[0], limit);
 
 	auto test = limit + "X";
 	lines = WordWrap(test);
-	ASSERT_EQ(lines.size(), 1);
-	ASSERT_EQ(lines[0], test);
+	REQUIRE_EQ(lines.size(), 1);
+	REQUIRE_EQ(lines[0], test);
 
 	test = limit + limit;
 	lines = WordWrap(test);
-	ASSERT_EQ(lines.size(), 1);
-	ASSERT_EQ(lines[0], test);
+	REQUIRE_EQ(lines.size(), 1);
+	REQUIRE_EQ(lines[0], test);
 
 	test = limit + " " + limit;
 	lines = WordWrap(test);
-	ASSERT_EQ(lines.size(), 2);
-	ASSERT_EQ(lines[0], limit);
-	ASSERT_EQ(lines[1], limit);
+	REQUIRE_EQ(lines.size(), 2);
+	REQUIRE_EQ(lines[0], limit);
+	REQUIRE_EQ(lines[1], limit);
 
 	test = limit + "  " + limit;
 	lines = WordWrap(test);
-	ASSERT_EQ(lines.size(), 2);
-	ASSERT_EQ(lines[0], limit);
-	ASSERT_EQ(lines[1], limit);
+	REQUIRE_EQ(lines.size(), 2);
+	REQUIRE_EQ(lines[0], limit);
+	REQUIRE_EQ(lines[1], limit);
 
 	test = limit.substr(0, limit.size()-1) + " " + limit;
 	lines = WordWrap(test);
-	ASSERT_EQ(lines.size(), 2);
-	ASSERT_EQ(lines[0], limit.substr(0, limit.size()-1));
-	ASSERT_EQ(lines[1], limit);
+	REQUIRE_EQ(lines.size(), 2);
+	REQUIRE_EQ(lines[0], limit.substr(0, limit.size()-1));
+	REQUIRE_EQ(lines[1], limit);
 
 }
