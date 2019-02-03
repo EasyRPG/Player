d8b7440fdde8c6b963b41fe2ff4f028453903bf7
Matthew Fioravante <fmatthew5876@gmail.com>
2019-02-02 19:20:44 -0500

    Fix text positions in status menu

--- src/window_actorinfo.cpp
+++ src/window_actorinfo.cpp
@@ -42,7 +42,7 @@ void Window_ActorInfo::Refresh() {
 void Window_ActorInfo::DrawInfo() {
 	// Draw Row formation.
 	std::string battle_row = Game_Actors::GetActor(actor_id)->GetBattleRow() == Game_Actor::RowType::RowType_back ? "Back" : "Front";
-	contents->TextDraw(contents->GetWidth(), 5, Font::ColorDefault, battle_row, Text::AlignRight);
+	contents->TextDraw(contents->GetWidth(), 2, Font::ColorDefault, battle_row, Text::AlignRight);
 
 	const Game_Actor& actor = *Game_Actors::GetActor(actor_id);
 
@@ -50,25 +50,22 @@ void Window_ActorInfo::DrawInfo() {
 	DrawActorFace(actor, 0, 0);
 
 	// Draw Name
-	contents->TextDraw(3, 50, 1, "Name");
-	DrawActorName(actor, 36, 65);
+	contents->TextDraw(0, 50, 1, "Name");
+	DrawActorName(actor, 36, 66);
 
 	// Draw Profession
 	contents->TextDraw(3, 80, 1, "Class");
-	DrawActorClass(actor, 36, 95);
+	DrawActorClass(actor, 36, 98);
 
 	// Draw Rank
 	contents->TextDraw(3, 110, 1, "Nickname");
-	DrawActorTitle(actor, 36, 125);
+	DrawActorTitle(actor, 36, 130);
 
 	// Draw Status
 	contents->TextDraw(3, 140, 1, "State");
-	DrawActorState(actor, 36, 155);
+	DrawActorState(actor, 36, 162);
 
 	//Draw Level
-	contents->TextDraw(3, 170, 1, Data::terms.level);
-	std::stringstream ss;
-	ss << actor.GetLevel();
-	contents->TextDraw(79, 170, Font::ColorDefault, ss.str(), Text::AlignRight);
-
+	contents->TextDraw(0, 178, 1, Data::terms.level);
+	contents->TextDraw(78, 178, Font::ColorDefault, std::to_string(actor.GetLevel()), Text::AlignRight);
 }
