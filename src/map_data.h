949d6deeb78f49d4dc12294c2873b0287ea0d6c2
Matthew Fioravante <fmatthew5876@gmail.com>
2019-07-14 21:34:38 -0400

    Terrain / Tile fixes
    * Add some new chipset BLOCK constants
    * Add function for chip id -> chip index mapping
    * If out of bounds coordinates are requested on non-looping map,
      RPG_RT will use the terrain of the first tile.
    * Fix bug where tile substitution for last few tiles
      was broken for terrain lookups. Fix: #1834

--- src/map_data.h
+++ src/map_data.h
@@ -20,16 +20,25 @@
 
 // Tile IDs
 
-static const int BLOCK_C = 3000;
+static constexpr int BLOCK_C = 3000;
+static constexpr int BLOCK_C_TILES = 3;
+static constexpr int BLOCK_C_INDEX = 3;
 
-static const int BLOCK_D = 4000;
-static const int BLOCK_D_BLOCKS = 12;
+static constexpr int BLOCK_D = 4000;
+static constexpr int BLOCK_D_TILES = 12;
+static constexpr int BLOCK_D_INDEX = 6;
 
-static const int BLOCK_E = 5000;
-static const int BLOCK_E_TILES = 144;
+static constexpr int BLOCK_E = 5000;
+static constexpr int BLOCK_E_TILES = 144;
+static constexpr int BLOCK_E_INDEX = 18;
 
-static const int BLOCK_F = 10000;
-static const int BLOCK_F_TILES = 144;
+static constexpr int BLOCK_F = 10000;
+static constexpr int BLOCK_F_TILES = 144;
+static constexpr int BLOCK_F_INDEX = 162;
+
+static constexpr int NUM_LOWER_TILES = BLOCK_F_INDEX;
+static constexpr int NUM_UPPER_TILES = BLOCK_F_TILES;
+static constexpr int NUM_TILES = NUM_LOWER_TILES + NUM_UPPER_TILES;
 
 /** Passability flags. */
 namespace Passable {
@@ -42,6 +51,25 @@ namespace Passable {
 		Wall		= 0x20,
 		Counter		= 0x40
 	};
+}
+
+inline int ChipIdToIndex(int chip_id) {
+	if (chip_id < BLOCK_C + 50) {
+		return chip_id / 1000;
+	}
+	else if (chip_id < BLOCK_D) {
+		return 4 + (chip_id - BLOCK_C - 50) / 50;
+	}
+	else if (chip_id < BLOCK_E) {
+		return 6 + (chip_id - BLOCK_D) / 50;
+	}
+	else if (chip_id < BLOCK_E + BLOCK_E_TILES) {
+		return 18 + (chip_id - BLOCK_E);
+	}
+	else if (chip_id < BLOCK_E + BLOCK_E_TILES) {
+		return 18 + (chip_id - BLOCK_E);
+	}
+	return 0;
 }
 
 #endif
