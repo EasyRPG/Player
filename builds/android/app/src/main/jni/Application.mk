5ed3667abb0a9a3a3de50115dff84ca12bb21077
Ghabry <gabriel+github@mastergk.de>
2020-03-31 13:04:07 +0200

    Android: Remove armeabi, add x86_64
    Fix #1911

--- builds/android/app/src/main/jni/Application.mk
+++ builds/android/app/src/main/jni/Application.mk
@@ -2,4 +2,4 @@
 # See CPLUSPLUS-SUPPORT.html in the NDK documentation for more information
 
 APP_STL := c++_static
-APP_ABI := armeabi armeabi-v7a x86 arm64-v8a
+APP_ABI := armeabi-v7a arm64-v8a x86 x86_64
