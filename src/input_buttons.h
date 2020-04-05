f5cab025ecabc0e4c9e6a3c98e97efb31fd77f55
Matthew Fioravante <fmatthew5876@gmail.com>
2020-04-05 00:17:52 -0400

    Add EnumTags for input keys

--- src/input_buttons.h
+++ src/input_buttons.h
@@ -21,6 +21,8 @@
 // Headers
 #include <vector>
 
+#include "enum_tags.h"
+
 /**
  * Input namespace.
  */
@@ -65,6 +67,85 @@ namespace Input {
 		TOGGLE_ZOOM,
 		BUTTON_COUNT
 	};
+
+	constexpr auto kButtonNames = makeEnumTags<InputButton>(
+		"UP",
+		"DOWN",
+		"LEFT",
+		"RIGHT",
+		"DECISION",
+		"CANCEL",
+		"SHIFT",
+		"N0",
+		"N1",
+		"N2",
+		"N3",
+		"N4",
+		"N5",
+		"N6",
+		"N7",
+		"N8",
+		"N9",
+		"PLUS",
+		"MINUS",
+		"MULTIPLY",
+		"DIVIDE",
+		"PERIOD",
+		"DEBUG_MENU",
+		"DEBUG_THROUGH",
+		"DEBUG_SAVE",
+		"TOGGLE_FPS",
+		"TAKE_SCREENSHOT",
+		"SHOW_LOG",
+		"RESET",
+		"PAGE_UP",
+		"PAGE_DOWN",
+		"SCROLL_UP",
+		"SCROLL_DOWN",
+		"FAST_FORWARD",
+		"TOGGLE_FULLSCREEN",
+		"TOGGLE_ZOOM",
+		"BUTTON_COUNT");
+
+	constexpr auto kButtonHelp = makeEnumTags<InputButton>(
+		"Up Direction",
+		"Down Direction",
+		"Left Direction",
+		"Right Direction",
+		"Decision Key",
+		"Cancel Key",
+		"Shift Key",
+		"Number 0",
+		"Number 1",
+		"Number 2",
+		"Number 3",
+		"Number 4",
+		"Number 5",
+		"Number 6",
+		"Number 7",
+		"Number 8",
+		"Number 9",
+		"Plus Key",
+		"Minus Key",
+		"Multiply Key",
+		"Divide Key",
+		"Period Key",
+		"(Test Play) Open the debug menu",
+		"(Test Play) Walk through walls",
+		"(Test Play) Open the save menu",
+		"Open the settings menu",
+		"Toggle the FPS display",
+		"Take a screenshot",
+		"Show the console log on the screen",
+		"Reset to the title screen",
+		"Page up key",
+		"Page down key",
+		"Scroll up key",
+		"Scroll down key",
+		"Fast forward key",
+		"Toggle Fullscreen mode",
+		"Toggle Window Zoom level",
+		"Total Button Count");
 
 	/**
 	 * Initializes input buttons to their mappings.
