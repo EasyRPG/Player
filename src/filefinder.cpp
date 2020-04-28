EasyRPG/Player

commit 7de54b62c1588797205e204ed481914e0f779f79

Make path adjustments less spammy.
The "Path adjusted" debug log was mostly useless and the file probing afterwards generated "File not found" messages because they called again in the FileFinder.
Now directory and name are only adjusted and the internal FileFinder call continues.

src/filefinder.cpp
@@ -140,18 +140,15 @@ namespace {
 		if (combined_path != canon) {
 			// Very few games (e.g. Yume2kki) use path traversal (..) in the filenames to point
 			// to files outside of the actual directory.
-			// Fix the path and search the file again with the correct root directory set.
-			if (dir != ".") {
-				// Prevent "path adjusted" debug log when searching for ExFont
-				Output::Debug("Path adjusted: %s -> %s", combined_path.c_str(), canon.c_str());
+			// Fix the path and continue searching.
+			size_t pos = canon.find_first_of("/");
+			if (pos == std::string::npos) {
+				corrected_dir = ".";
+				corrected_name = canon;
+			} else {
+				corrected_dir = canon.substr(0, pos);
+				corrected_name = canon.substr(pos + 1);
 			}
-			for (char const** c = exts; *c != NULL; ++c) {
-				std::string res = FileFinder::FindDefault(tree, canon + *c);
-				if (!res.empty()) {
-					return res;
-				}
-			}
-			return "";
 		}
 
 #ifdef _WIN32

commit 3aa8b5f2b53873dc3bed6c9a23c4b7e7e3f596a2

Fix canonical path logic not working anymore when the path is the root directory.
The FileFinder data structure stores files in the root dir at a different location, call into FindDefault to solve this.

Fix #2176

Regression caused by 7de54b62

src/filefinder.cpp
@@ -110,8 +110,13 @@ namespace {
 			// Fix the path and continue searching.
 			size_t pos = canon.find_first_of("/");
 			if (pos == std::string::npos) {
-				corrected_dir = ".";
-				corrected_name = canon;
+				for (char const** c = exts; *c != NULL; ++c) {
+					std::string res = FindDefault(tree, canon + *c);
+					if (!res.empty()) {
+						return res;
+					}
+				}
+				return "";
 			} else {
 				corrected_dir = canon.substr(0, pos);
 				corrected_name = canon.substr(pos + 1);