EasyRPG/Player

commit 1b6b7d97010b660793a822d9b9512c43be9722e8

LSD compatible fixes for SavePicture::current_bot_trans
* RPG2k - Set only top trans chunk, disable feature
* RPG2k3 - Set both chunks independently, enable feature
* RPG2k3e - Set both chunks the same (from editor, don't force it in
code), disable feature

src/game_interpreter.cpp
@@ -2407,40 +2407,39 @@ bool Game_Interpreter::CommandShowPicture(RPG::EventCommand const& com) { // cod
 
 	size_t param_size = com.parameters.size();
 
-	if (Player::IsRPG2k() || Player::IsRPG2k3E()) {
-		if (param_size > 16) {
-			// Handling of RPG2k3 1.12 chunks
-			pic_id = ValueOrVariable(com.parameters[17], pic_id);
-			if (com.parameters[19] != 0) {
-				int var = 0;
-				if (Game_Variables.IsValid(com.parameters[19])) {
-					var = Game_Variables.Get(com.parameters[19]);
-				}
-				params.name = PicPointerPatch::ReplaceName(params.name, var, com.parameters[18]);
-			}
-			params.magnify = ValueOrVariable(com.parameters[20], params.magnify);
-			params.top_trans = ValueOrVariable(com.parameters[21], params.top_trans);
-			params.spritesheet_cols = com.parameters[22];
-			params.spritesheet_rows = com.parameters[23];
+	if (param_size > 14) {
+		// RPG2k3 sets this chunk. Versions < 1.12 let you specify separate top and bottom
+		// transparency. >= 1.12 Editor only let you set one transparency field but it affects
+		// both chunks here.
+		params.bottom_trans = com.parameters[14];
+	}
 
-			// Animate and index selection are exclusive
-			if (com.parameters[24] == 2) {
-				params.spritesheet_speed = com.parameters[25];
-			} else {
-				params.spritesheet_frame = ValueOrVariable(com.parameters[24], com.parameters[25]);
+	if (param_size > 16) {
+		// Handling of RPG2k3 1.12 chunks
+		pic_id = ValueOrVariable(com.parameters[17], pic_id);
+		if (com.parameters[19] != 0) {
+			int var = 0;
+			if (Game_Variables.IsValid(com.parameters[19])) {
+				var = Game_Variables.Get(com.parameters[19]);
 			}
+			params.name = PicPointerPatch::ReplaceName(params.name, var, com.parameters[18]);
+		}
+		params.magnify = ValueOrVariable(com.parameters[20], params.magnify);
+		params.top_trans = ValueOrVariable(com.parameters[21], params.top_trans);
+		params.spritesheet_cols = com.parameters[22];
+		params.spritesheet_rows = com.parameters[23];
 
-			params.spritesheet_loop = !com.parameters[26];
-			params.map_layer = com.parameters[27];
-			params.battle_layer = com.parameters[28];
-			params.flags = com.parameters[29];
+		// Animate and index selection are exclusive
+		if (com.parameters[24] == 2) {
+			params.spritesheet_speed = com.parameters[25];
+		} else {
+			params.spritesheet_frame = ValueOrVariable(com.parameters[24], com.parameters[25]);
 		}
 
-		// RPG2k and RPG2k3 1.10 do not support this option
-		params.bottom_trans = params.top_trans;
-	} else {
-		// Corner case when 2k maps are used in 2k3 (pre-1.10) and don't contain this chunk
-		params.bottom_trans = param_size > 14 ? com.parameters[14] : params.top_trans;
+		params.spritesheet_loop = !com.parameters[26];
+		params.map_layer = com.parameters[27];
+		params.battle_layer = com.parameters[28];
+		params.flags = com.parameters[29];
 	}
 
 	PicPointerPatch::AdjustShowParams(pic_id, params);

src/game_picture.cpp
@@ -108,11 +108,20 @@ void Game_Picture::UpdateSprite() {
 	sprite->SetWaverPhase(data.effect_mode == RPG::SavePicture::Effect_wave ? data.current_waver * 360 / 256 : 0.0);
 	sprite->SetWaverDepth(data.effect_mode == RPG::SavePicture::Effect_wave ? data.current_effect_power * 2 : 0);
 
+	// Only older versions of RPG_RT apply the effects of current_bot_trans chunk.
+	const bool use_bottom_trans = (Player::IsRPG2k3() && !Player::IsRPG2k3E());
+	const auto top_trans = data.current_top_trans;
+	const auto bottom_trans = use_bottom_trans ? data.current_bot_trans : top_trans;
+
 	sprite->SetOpacity(
-		(int)(255 * (100 - data.current_top_trans) / 100),
-		(int)(255 * (100 - data.current_bot_trans) / 100));
-	if (data.current_bot_trans != data.current_top_trans)
+		(int)(255 * (100 - top_trans) / 100),
+		(int)(255 * (100 - bottom_trans) / 100));
+
+	if (bottom_trans != top_trans) {
 		sprite->SetBushDepth(sprite->GetHeight() / 2);
+	} else {
+		sprite->SetBushDepth(0);
+	}
 
 	auto tone = Tone((int) (data.current_red * 128 / 100),
 			(int) (data.current_green * 128 / 100),

commit d87fbb3e90cd98776b44b5aafb8d5f10f346ffef

Regression fix: Reintroduce edge-case: When a 2k ShowPicture command is used in 2k3<1.10 the top_trans must be copied into bottom_trans.
Regression caused by 1b6b7d9

Fix #2178

src/game_interpreter.cpp
@@ -2504,6 +2504,9 @@ bool Game_Interpreter::CommandShowPicture(RPG::EventCommand const& com) { // cod
 		// transparency. >= 1.12 Editor only let you set one transparency field but it affects
 		// both chunks here.
 		params.bottom_trans = com.parameters[14];
+	} else if (Player::IsRPG2k3() && !Player::IsRPG2k3E()) {
+		// Corner case when 2k maps are used in 2k3 (pre-1.10) and don't contain this chunk
+		params.bottom_trans = params.top_trans;
 	}
 
 	if (param_size > 16) {