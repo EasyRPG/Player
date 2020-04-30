6f1d8f7d14bd11e6aacd1602b20303b098e37efe
Matthew Fioravante <fmatthew5876@gmail.com>
2020-04-30 11:51:37 -0400

    Fix output test and vita

--- tests/output.cpp
+++ tests/output.cpp
@@ -8,9 +8,9 @@ TEST_SUITE_BEGIN("Output");
 TEST_CASE("Message Output") {
 	Graphics::Init();
 	Main_Data::Init();
-	Output::Debug("Test %s", "debg");
-	Output::Warning("Test %s", "test");
-	Output::Post("Test %s", "post");
+	Output::Debug("Test {}", "debg");
+	Output::Warning("Test {}", "test");
+	Output::Info("Test {}", "info");
 	Main_Data::Cleanup();
 	Graphics::Quit();
 }
