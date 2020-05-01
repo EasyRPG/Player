c415648965e51c16b99151089d8ddba7282d429f
Matthew Fioravante <fmatthew5876@gmail.com>
2020-05-01 13:23:06 -0400

    Add Wii Platform Clock

--- src/platform/wii/wii_clock.cpp
+++ src/platform/wii/wii_clock.cpp
@@ -14,29 +14,9 @@
  * You should have received a copy of the GNU General Public License
  * along with EasyRPG Player. If not, see <http://www.gnu.org/licenses/>.
  */
+#include "wii_clock.h"
 
-#ifndef EP_PLATFORM_CLOCK_H
-#define EP_PLATFORM_CLOCK_H
-
-#include <chrono>
-#include <type_traits>
-#include <thread>
-
-#if defined(_3DS)
-#include "platform/3ds/3ds_clock.h"
-using Platform_Clock = CtrClock;
-#elif defined(GEKKO)
-#include "platform/wii/wii_clock.h"
-using Platform_Clock = WiiClock;
-#elif defined(__SWITCH__)
-#include "platform/switch/switch_clock.h"
-using Platform_Clock = NxClock;
-#elif defined(PSP2)
-#include "platform/psvita/psp2_clock.h"
-using Platform_Clock = Psp2Clock;
-#else
-#include "std_clock.h"
-using Platform_Clock = StdClock;
-#endif
-
+#ifdef GEKKO
+constexpr bool WiiClock::is_steady;
+constexpr int64_t WiiClock::ticks_per_sec;
 #endif
