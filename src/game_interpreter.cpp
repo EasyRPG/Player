aed6fccb70e1f698df86527e686fdab25655df33
Matthew Fioravante <fmatthew5876@gmail.com>
2020-05-24 13:31:39 -0400

    Fix spritesheet save chunks when loading picture with no sheet
    Fix: #2226

--- src/game_interpreter.cpp
+++ src/game_interpreter.cpp
@@ -2519,18 +2519,21 @@ bool Game_Interpreter::CommandShowPicture(RPG::EventCommand const& com) { // cod
 		}
 		params.magnify = ValueOrVariable(com.parameters[20], params.magnify);
 		params.top_trans = ValueOrVariable(com.parameters[21], params.top_trans);
-		params.spritesheet_cols = com.parameters[22];
-		params.spritesheet_rows = com.parameters[23];
+		if (com.parameters[22] > 0) {
+			// If spritesheet is enabled
+			params.spritesheet_cols = com.parameters[22];
+			params.spritesheet_rows = com.parameters[23];
 
-		// Animate and index selection are exclusive
-		if (com.parameters[24] == 2) {
-			params.spritesheet_speed = com.parameters[25];
-		} else {
-			// Picture data / LSD data frame number is 0 based, while event parameter counts from 1.
-			params.spritesheet_frame = ValueOrVariable(com.parameters[24], com.parameters[25]) - 1;
+			// Animate and index selection are exclusive
+			if (com.parameters[24] == 2) {
+				params.spritesheet_speed = com.parameters[25];
+				params.spritesheet_play_once = com.parameters[26];
+			} else {
+				// Picture data / LSD data frame number is 0 based, while event parameter counts from 1.
+				params.spritesheet_frame = ValueOrVariable(com.parameters[24], com.parameters[25]) - 1;
+			}
 		}
 
-		params.spritesheet_play_once = com.parameters[26];
 		params.map_layer = com.parameters[27];
 		params.battle_layer = com.parameters[28];
 		params.flags = com.parameters[29];
