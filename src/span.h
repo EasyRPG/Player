d8f4c1d6faa736a6ee65302926316441f5cbb174
Matthew Fioravante <fmatthew5876@gmail.com>
2020-08-19 16:22:23 -0400

    Add Span from liblcf

---
+++ src/span.h
@@ -0,0 +1,28 @@
+/*
+ * This file is part of EasyRPG Player.
+ *
+ * EasyRPG Player is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * EasyRPG Player is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with EasyRPG Player. If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#ifndef EP_SPAN_H
+#define EP_SPAN_H
+
+#include <lcf/span.h>
+
+using lcf::Span;
+using lcf::ExtentT;
+using lcf::dynamic_extent;
+using lcf::MakeSpan;
+
+#endif
