90c3fa5f89eae1237dd341c422eafade72175afe
Francisco de la Peña <fran@fran.cr>
2020-08-20 21:55:30 -0600

    Change Warning to Debug on SE playback failure This is workaround for #1356 because users still complain about this nag.

--- src/audio_generic.cpp
+++ src/audio_generic.cpp
@@ -153,8 +153,8 @@ void GenericAudio::SE_Play(std::string const &file, int volume, int pitch) {
 			return;
 		}
 	}
-
-	Output::Warning("Couldn't play %s SE. No free channel available", FileFinder::GetPathInsideGamePath(file).c_str());
+	// FIXME Not displaying as warning because multiple games exhaust free channels available, see #1356
+	Output::Debug("Couldn't play %s SE. No free channel available", FileFinder::GetPathInsideGamePath(file).c_str());
 }
 
 void GenericAudio::SE_Stop() {
