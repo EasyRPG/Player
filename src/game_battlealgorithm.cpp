601df8ec3ef81398160b15b0d61642fb0466ca32
rueter37 <61623769+rueter37@users.noreply.github.com>
2021-02-15 15:45:18 +0100

    Add option to enable stat absorbing
    Absorbing attack, defense, spirit and agility can now be enabled
    in the database.

--- src/game_battlealgorithm.cpp
+++ src/game_battlealgorithm.cpp
@@ -973,6 +973,7 @@ bool Game_BattleAlgorithm::Skill::vExecute() {
 		const auto atk = target->CanChangeAtkModifier(effect);
 		if (atk != 0) {
 			SetAffectedAtk(atk);
+			if (lcf::Data::system.easyrpg_enable_stat_absorbing) SetIsAbsorbAtk(absorb);
 			SetIsSuccess();
 		}
 	}
@@ -980,6 +981,7 @@ bool Game_BattleAlgorithm::Skill::vExecute() {
 		const auto def = target->CanChangeDefModifier(effect);
 		if (def != 0) {
 			SetAffectedDef(def);
+			if (lcf::Data::system.easyrpg_enable_stat_absorbing) SetIsAbsorbDef(absorb);
 			SetIsSuccess();
 		}
 	}
@@ -987,6 +989,7 @@ bool Game_BattleAlgorithm::Skill::vExecute() {
 		const auto spi = target->CanChangeSpiModifier(effect);
 		if (spi != 0) {
 			SetAffectedSpi(spi);
+			if (lcf::Data::system.easyrpg_enable_stat_absorbing) SetIsAbsorbSpi(absorb);
 			SetIsSuccess();
 		}
 	}
@@ -994,6 +997,7 @@ bool Game_BattleAlgorithm::Skill::vExecute() {
 		const auto agi = target->CanChangeAgiModifier(effect);
 		if (agi != 0) {
 			SetAffectedAgi(agi);
+			if (lcf::Data::system.easyrpg_enable_stat_absorbing) SetIsAbsorbAgi(absorb);
 			SetIsSuccess();
 		}
 	}
