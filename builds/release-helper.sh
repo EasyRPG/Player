fc460724a17a30c7d7689a13dea584a7c83509a2
Carsten Teibes <dev@f4ke.de>
2022-04-26 02:00:51 +0200

    Update release-helper script for latest changes

--- builds/release-helper.sh
+++ builds/release-helper.sh
@@ -21,13 +21,21 @@ if [[ ! $version =~ ^[0-9]\.[0-9]\.[0-9]$ ]]; then
 
 fi
 
+# convenience variables
+_maj=${version%%.*}
+_pat=${version##*.}
+_min=${version%.*}
+_min=${_min#*.}
+
 echo "Updating Version in:"
 
 echo "  CMakeLists.txt"
 sed -i "/EasyRPG_Player VERSION/,1 s/[0-9]\.[0-9]\.[0-9]/$version/" CMakeLists.txt
 
 echo "  configure.ac"
-sed -i "/AC_INIT/,1 s/[0-9]\.[0-9]\.[0-9]/$version/" configure.ac
+sed -i -e "/ep_version_major/,1 s/\[[0-9]\+\]/[$_maj]/" \
+       -e "/ep_version_minor/,1 s/\[[0-9]\+\]/[$_min]/" \
+       -e "/ep_version_patch/,1 s/\[[0-9]\+\]/[$_pat]/" configure.ac
 
 echo "  builds/android/gradle.properties"
 _android_commits=`git rev-list HEAD --count`
@@ -37,7 +45,7 @@ sed -i -e "/VERSION_NAME/,1 s/[0-9]\.[0-9]\.[0-9]/$version/" \
 echo "  resources/wii/meta.xml"
 _wiidate=$(date +%Y%m%d000000)
 sed -i -e "/version/,1 s/[0-9]\.[0-9]\.[0-9]/$version/" \
-       -e "/release_date/,1 s/[0-9]\{14\}/$_wiidate/" resourses/wii/meta.xml
+       -e "/release_date/,1 s/[0-9]\{14\}/$_wiidate/" resources/wii/meta.xml
 
 echo "  resources/psvita/template.xml"
 sed -i "/EasyRPG Player/,1 s/[0-9]\.[0-9]\.[0-9]/$version/" resources/psvita/template.xml
@@ -50,14 +58,10 @@ echo "  resources/player.rc"
 sed -i "s/[0-9]\.[0-9]\.[0-9]/$version/; s/[0-9],[0-9],[0-9]/${version//./,}/" \
   resources/player.rc
 
-echo "  src/version.h"
-_maj=${version%%.*}
-_pat=${version##*.}
-_min=${version%.*}
-_min=${_min#*.}
-sed -i -e "s/\(#define PLAYER_MAJOR\).*/\1 $_maj/" \
-       -e "s/\(#define PLAYER_MINOR\).*/\1 $_min/" \
-       -e "s/\(#define PLAYER_PATCH\).*/\1 $_pat/" src/version.h
+echo "  src/version.cpp"
+sed -i -e "s/\(#define EP_VERSION_MAJOR\).*/\1 $_maj/" \
+       -e "s/\(#define EP_VERSION_MINOR\).*/\1 $_min/" \
+       -e "s/\(#define EP_VERSION_PATCH\).*/\1 $_pat/" src/version.cpp
 
 echo "  README.md"
 sed -i "s/\(easyrpg-player-\)[0-9]\.[0-9]\.[0-9]/\1$version/g" README.md
