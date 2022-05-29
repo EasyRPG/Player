c281773677dc3457ed083e61559332d4ee268d72
Ghabry <gabriel+github@mastergk.de>
2022-05-29 21:07:12 +0200

    Maniac: Bug fix & Clean up expression parser

--- src/maniac_patch.cpp
+++ src/maniac_patch.cpp
@@ -75,15 +75,17 @@ namespace {
 		Ternary = 72,
 		Function = 78
 	};
-
-	using Result = std::tuple<std::vector<uint32_t>::iterator, int>;
 }
 
-Result process(std::vector<uint32_t>::iterator it, std::vector<uint32_t>::iterator end) {
+int process(std::vector<int32_t>::iterator& it, std::vector<int32_t>::iterator end) {
 	int value = 0;
 	int imm = 0;
 	int imm2 = 0;
 	int imm3 = 0;
+
+	if (it == end) {
+		return 0;
+	}
 
 	auto op = static_cast<Op>(*it);
 	++it;
@@ -91,148 +93,164 @@ Result process(std::vector<uint32_t>::iterator it, std::vector<uint32_t>::iterat
 	// When entering the switch it is on the first argument
 	switch (op) {
 		case Op::Null:
-			return {++it, 0};
+			*it++;
+			return 0;
 		case Op::U8:
 		case Op::UX8:
 			value = *it++;
-			return {it, static_cast<uint8_t>(value)};
+			return value;
 		case Op::U16:
 		case Op::UX16:
 			imm = *it++;
+			if (it == end) {
+				return 0;
+			}
 			imm2 = *it++;
 			value = (imm2 << 8) + imm;
-			return {it, static_cast<uint16_t>(value)};
+			return value;
 		case Op::S32:
 		case Op::SX32:
 			imm = *it++;
+			if (it == end) {
+				return 0;
+			}
 			imm2 = *it++;
+			if (it == end) {
+				return 0;
+			}
 			imm3 = *it++;
+			if (it == end) {
+				return 0;
+			}
 			value = *it++;
 			value = (value << 24) + (imm3 << 16) + (imm2 << 8) + imm;
-			return {it, value};
+			return value;
 		case Op::Var:
-			std::tie(it, imm) = process(it, end);
-			return {it, Main_Data::game_variables->Get(imm)};
+			imm = process(it, end);
+			return Main_Data::game_variables->Get(imm);
 		case Op::Switch:
-			std::tie(it, imm) = process(it, end);
-			return {it, Main_Data::game_switches->GetInt(imm)};
+			imm = process(it, end);
+			return Main_Data::game_switches->GetInt(imm);
 		case Op::VarIndirect:
-			std::tie(it, imm) = process(it, end);
-			return {it, Main_Data::game_variables->GetIndirect(imm)};
+			imm = process(it, end);
+			return Main_Data::game_variables->GetIndirect(imm);
 		case Op::SwitchIndirect:
-			std::tie(it, imm) = process(it, end);
-			return {it, Main_Data::game_switches->GetInt(Main_Data::game_variables->Get(imm))};
+			imm = process(it, end);
+			return Main_Data::game_switches->GetInt(Main_Data::game_variables->Get(imm));
 		case Op::Negate:
-			std::tie(it, imm) = process(it, end);
-			return {it, -imm};
+			imm = process(it, end);
+			return -imm;
 		case Op::Not:
-			std::tie(it, imm) = process(it, end);
-			return {it, !imm ? 0 : 1};
+			imm = process(it, end);
+			return !imm ? 0 : 1;
 		case Op::Flip:
-			std::tie(it, imm) = process(it, end);
-			return {it, ~imm};
+			imm = process(it, end);
+			return ~imm;
 		case Op::Add:
-			std::tie(it, imm) = process(it, end);
-			std::tie(it, imm2) = process(it, end);
-			return {it, imm + imm2};
+			imm = process(it, end);
+			imm2 = process(it, end);
+			return static_cast<int32_t>(Utils::Clamp<int64_t>(static_cast<int64_t>(imm) + imm2, std::numeric_limits<int32_t>::min(), std::numeric_limits<int32_t>::max()));
 		case Op::Sub:
-			std::tie(it, imm) = process(it, end);
-			std::tie(it, imm2) = process(it, end);
-			return {it, imm - imm2};
+			imm = process(it, end);
+			imm2 = process(it, end);
+			return static_cast<int32_t>(Utils::Clamp<int64_t>(static_cast<int64_t>(imm) - imm2, std::numeric_limits<int32_t>::min(), std::numeric_limits<int32_t>::max()));
 		case Op::Mul:
-			std::tie(it, imm) = process(it, end);
-			std::tie(it, imm2) = process(it, end);
-			return {it, imm * imm2};
+			imm = process(it, end);
+			imm2 = process(it, end);
+			return static_cast<int32_t>(Utils::Clamp<int64_t>(static_cast<int64_t>(imm) * imm2, std::numeric_limits<int32_t>::min(), std::numeric_limits<int32_t>::max()));
 		case Op::Div:
-			std::tie(it, imm) = process(it, end);
-			std::tie(it, imm2) = process(it, end);
+			imm = process(it, end);
+			imm2 = process(it, end);
 			if (imm2 == 0) {
-				return {it, 0};
+				return imm;
 			}
-			return {it, imm / imm2};
+			return imm / imm2;
 		case Op::Mod:
-			std::tie(it, imm) = process(it, end);
-			std::tie(it, imm2) = process(it, end);
+			imm = process(it, end);
+			imm2 = process(it, end);
 			if (imm2 == 0) {
-				return {it, 0};
+				return imm;
 			}
-			return {it, imm % imm2};
+			return imm % imm2;
 		case Op::BitOr:
-			std::tie(it, imm) = process(it, end);
-			std::tie(it, imm2) = process(it, end);
-			return {it, imm | imm2};
+			imm = process(it, end);
+			imm2 = process(it, end);
+			return imm | imm2;
 		case Op::BitAnd:
-			std::tie(it, imm) = process(it, end);
-			std::tie(it, imm2) = process(it, end);
-			return {it, imm & imm2};
+			imm = process(it, end);
+			imm2 = process(it, end);
+			return imm & imm2;
 		case Op::BitXor:
-			std::tie(it, imm) = process(it, end);
-			std::tie(it, imm2) = process(it, end);
-			return {it, imm ^ imm2};
+			imm = process(it, end);
+			imm2 = process(it, end);
+			return imm ^ imm2;
 		case Op::BitShiftLeft:
-			std::tie(it, imm) = process(it, end);
-			std::tie(it, imm2) = process(it, end);
-			return {it, imm << imm2};
+			imm = process(it, end);
+			imm2 = process(it, end);
+			return imm << imm2;
 		case Op::BitShiftRight:
-			std::tie(it, imm) = process(it, end);
-			std::tie(it, imm2) = process(it, end);
-			return {it, imm >> imm2};
+			imm = process(it, end);
+			imm2 = process(it, end);
+			return imm >> imm2;
 		case Op::Equal:
-			std::tie(it, imm) = process(it, end);
-			std::tie(it, imm2) = process(it, end);
-			return {it, imm == imm2 ? 1 : 0};
+			imm = process(it, end);
+			imm2 = process(it, end);
+			return imm == imm2 ? 1 : 0;
 		case Op::GreaterEqual:
-			std::tie(it, imm) = process(it, end);
-			std::tie(it, imm2) = process(it, end);
-			return {it, imm >= imm2 ? 1 : 0};
+			imm = process(it, end);
+			imm2 = process(it, end);
+			return imm >= imm2 ? 1 : 0;
 		case Op::LessEqual:
-			std::tie(it, imm) = process(it, end);
-			std::tie(it, imm2) = process(it, end);
-			return {it, imm <= imm2 ? 1 : 0};
+			imm = process(it, end);
+			imm2 = process(it, end);
+			return imm <= imm2 ? 1 : 0;
 		case Op::Greater:
-			std::tie(it, imm) = process(it, end);
-			std::tie(it, imm2) = process(it, end);
-			return {it, imm > imm2 ? 1 : 0};
+			imm = process(it, end);
+			imm2 = process(it, end);
+			return imm > imm2 ? 1 : 0;
 		case Op::Less:
-			std::tie(it, imm) = process(it, end);
-			std::tie(it, imm2) = process(it, end);
-			return {it, imm < imm2 ? 1 : 0};
+			imm = process(it, end);
+			imm2 = process(it, end);
+			return imm < imm2 ? 1 : 0;
 		case Op::NotEqual:
-			std::tie(it, imm) = process(it, end);
-			std::tie(it, imm2) = process(it, end);
-			return {it, imm != imm2 ? 1 : 0};
+			imm = process(it, end);
+			imm2 = process(it, end);
+			return imm != imm2 ? 1 : 0;
 		case Op::Or:
-			std::tie(it, imm) = process(it, end);
-			std::tie(it, imm2) = process(it, end);
-			return {it, !!imm || !!imm2 ? 1 : 0};
+			imm = process(it, end);
+			imm2 = process(it, end);
+			return !!imm || !!imm2 ? 1 : 0;
 		case Op::And:
-			std::tie(it, imm) = process(it, end);
-			std::tie(it, imm2) = process(it, end);
-			return {it, !!imm && !!imm2 ? 1 : 0};
+			imm = process(it, end);
+			imm2 = process(it, end);
+			return !!imm && !!imm2 ? 1 : 0;
 		case Op::Subscript:
-			std::tie(it, imm) = process(it, end);
-			std::tie(it, imm2) = process(it, end);
-			return {it, 0};
+			// TODO
+			imm = process(it, end);
+			imm2 = process(it, end);
+			return 0;
 		case Op::Ternary:
-			std::tie(it, imm) = process(it, end);
-			std::tie(it, imm2) = process(it, end);
-			std::tie(it, imm3) = process(it, end);
-			return {it, imm != 0 ? imm2 : imm3};
+			imm = process(it, end);
+			imm2 = process(it, end);
+			imm3 = process(it, end);
+			return imm != 0 ? imm2 : imm3;
 		default:
 			Output::Warning("Maniac: Expression contains unsupported operation {}", static_cast<int>(op));
-			return {end, 0};
+			return 0;
 	}
 }
 
 int32_t ManiacPatch::ParseExpression(Span<const int32_t> op_codes) {
-	std::vector<uint32_t> ops;
+	std::vector<int32_t> ops;
 	for (auto &o: op_codes) {
-		ops.push_back((o & 0x000000FF));
-		ops.push_back((o & 0x0000FF00) >> 8);
-		ops.push_back((o & 0x00FF0000) >> 16);
-		ops.push_back((o & 0xFF000000) >> 24);
+		auto uo = static_cast<uint32_t>(o);
+		ops.push_back(static_cast<int32_t>(uo & 0x000000FF));
+		ops.push_back(static_cast<int32_t>((uo & 0x0000FF00) >> 8));
+		ops.push_back(static_cast<int32_t>((uo & 0x00FF0000) >> 16));
+		ops.push_back(static_cast<int32_t>((uo & 0xFF000000) >> 24));
 	}
-	return std::get<int>(process(ops.begin(), ops.end()));
+	auto beg = ops.begin();
+	return process(beg, ops.end());
 }
 
 std::array<bool, 50> ManiacPatch::GetKeyRange() {
