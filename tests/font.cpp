a93e9d2e63b51f0f67cfb013f45ddb78f1d35c56
Ghabry <gabriel+github@mastergk.de>
2022-11-14 19:29:40 +0100

    Adopt benchmarks and unit tests to new Font/Text API
    Most string tests removed because this API is gone.

--- tests/font.cpp
+++ tests/font.cpp
@@ -15,21 +15,10 @@ constexpr int ch = 12;
 constexpr int cwh = 6;
 constexpr int cwf = 12;
 
-TEST_CASE("FontSizeStr") {
-	auto font = Font::Default();
-
-	REQUIRE_EQ(font->GetSize(""), Rect(0, 0, 0, ch));
-	REQUIRE_EQ(font->GetSize(" "), Rect(0, 0, cwh, ch));
-	REQUIRE_EQ(font->GetSize("\n"), Rect(0, 0, 0, ch));
-	REQUIRE_EQ(font->GetSize("$A"), Rect(0, 0, cwh * 2, ch));
-	REQUIRE_EQ(font->GetSize("X\nX"), Rect(0, 0, cwh * 2, ch));
-	REQUIRE_EQ(font->GetSize("下"), Rect(0, 0, cwf, ch));
-}
-
 TEST_CASE("FontSizeChar") {
 	auto font = Font::Default();
 
-	REQUIRE_EQ(font->GetSize(0), Rect(0, 0, 0, ch));
+	REQUIRE_EQ(font->GetSize(0), Rect(0, 0, 0, 0));
 	REQUIRE_EQ(font->GetSize(U' '), Rect(0, 0, cwh, ch));
 	REQUIRE_EQ(font->GetSize(U'\n'), Rect(0, 0, 0, ch));
 	REQUIRE_EQ(font->GetSize(U'X'), Rect(0, 0, cwh, ch));
@@ -37,18 +26,10 @@ TEST_CASE("FontSizeChar") {
 	REQUIRE_EQ(font->GetSize(U'下'), Rect(0, 0, cwf, ch));
 }
 
-TEST_CASE("FontSizeStrEx") {
-	auto font = Font::exfont;
-
-	for (char i = 0; i < 52; ++i) {
-		REQUIRE_EQ(font->GetSize(std::string(1,i)), Rect(0, 0, cwf, cwf));
-	}
-}
-
 TEST_CASE("FontSizeCharEx") {
 	auto font = Font::exfont;
 
-	for (char32_t i = 0; i < 52; ++i) {
+	for (char32_t i = 32; i < 32 + 52; ++i) {
 		REQUIRE_EQ(font->GetSize(i), Rect(0, 0, cwf, cwf));
 	}
 }
@@ -57,14 +38,13 @@ TEST_CASE("FontGlyphChar") {
 	Bitmap::SetFormat(format_R8G8B8A8_a().format());
 	auto font = Font::Default();
 	auto check = [&](char32_t ch, Point p) {
-		auto ret = font->Glyph(ch);
+		auto ret = font->vRender(ch);
 		REQUIRE(ret.bitmap != nullptr);
 		REQUIRE_EQ(ret.advance, p);
 	};
 
-	check(0, Point(0, 0));
 	check(U' ', Point(cwh, 0));
-	check(U'\n', Point(0, 0));
+	check(U'\n', Point(cwh, 0));
 	check(U'X', Point(cwh, 0));
 	check(U'ぽ', Point(cwf, 0));
 	check(U'下', Point(cwf, 0));
@@ -75,7 +55,7 @@ TEST_CASE("FontGlyphCharEx") {
 	auto font = Font::exfont;
 
 	for (char32_t i = 0; i < 52; ++i) {
-		auto ret = font->Glyph(i);
+		auto ret = font->vRender(i);
 		REQUIRE(ret.bitmap != nullptr);
 		REQUIRE_EQ(ret.advance, Point(cwf, 0));
 	}
@@ -106,8 +86,8 @@ TEST_CASE("FontGlyphCharEx") {
 	auto surface = Bitmap::Create(width, height);
 	int color = 0;
 
-	for (char32_t i = 0; i < 52; ++i) {
-		REQUIRE_EQ(font->Render(*surface, 0, 0, *system, color, ch), Point(cwf, 0));
+	for (char32_t i = 32; i < 32 + 52; ++i) {
+		REQUIRE_EQ(font->Render(*surface, 0, 0, *system, color, i), Point(cwf, 0));
 	}
 }
 
