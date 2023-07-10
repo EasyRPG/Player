98fbd835587f8eeb151bb17fb66c7e2599865bc4
MonokoTech <137059552+monokotech@users.noreply.github.com>
2023-07-11 01:55:36 +0800

    EPMP: Add comments in Packet::Sanitize()

--- src/multiplayer/packet.cpp
+++ src/multiplayer/packet.cpp
@@ -12,8 +12,9 @@ std::string Packet::ToBytes() const {
 	return Build();
 }
 
-// guess 50%: this function will perform a parse of data with DELIMs
-// and the copied data is unchanged
+// this function will perform a parse of data with DELIMs
+// delimiters will be removed, and returns the copied data
+// To The Previous Participant: please please add more helpful comments!!
 constexpr std::string_view keywords[] = {
 	Packet::PARAM_DELIM,
 	Packet::MSG_DELIM,
@@ -25,11 +26,12 @@ std::string Packet::Sanitize(std::string_view param) {
 	std::bitset<k_size> searching_marks;
 	size_t candidate_index{};
 	for (size_t i = 0; i != param.size(); ++i) {
-		// append data before DELIMs
+		// append data until a potential DELIMs
 		if (candidate_index == 0) {
 			bool found = false;
 			for (size_t j = 0; j != k_size; ++j) {
 				assert(!keywords[j].empty());
+				// found a delimiter character but not sure it is
 				if (keywords[j][0] == param[i]) {
 					searching_marks.set(j);
 					found = true;
@@ -40,37 +42,42 @@ std::string Packet::Sanitize(std::string_view param) {
 				candidate_index = 1;
 			else
 				r += param[i];
-		// append DELIMs
+		// further confirmation to find delimiters
 		} else {
 			bool found = false;
 			bool match = false;
 			for (size_t j = 0; j != k_size; ++j) {
 				if (searching_marks.test(j)) {
+					// found sequential delims but it's not complete yet
 					if (keywords[j][candidate_index] == param[i]) {
 						found = true;
+						// the length not only 3 (such an \uFFFE), it can be more
+						// equal length means a complete match
 						if (keywords[j].size() == candidate_index + 1) {
 							match = true;
 							break;
 						}
 					} else {
+						// I'm sure it's not, do characters append
 						searching_marks.reset(j);
 					}
 				}
 			}
 
-			// guess 15%: skip the previous DELIMs until the last DELIM
+			// the i-char data is sub delimiter. skip the append
 			if (match) {
 				candidate_index = 0;
+			// incomplete match, keep trying
 			} else if (found) {
 				++candidate_index;
 			} else {
-				// append DELIMs encountered
+				// append previous pretended DELIMs
 				r.append(param.substr(i - candidate_index, candidate_index + 1));
 				candidate_index = 0;
 			}
 		}
 	}
-	// guess 99%: append remaining incomplete DELIMs
+	// remaining data are pretended DELIMs. append
 	if (candidate_index != 0)
 		r.append(param.substr(param.length() - candidate_index));
 	return r;
