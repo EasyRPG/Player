6cb6872f7895dcd180000abd7706c05efe463482
MonokoTech <137059552+monokotech@users.noreply.github.com>
2023-09-14 23:18:42 +0800

    EPMP
    - Add encryption functions
    - Add cryptopp.patch
    - Update CMakeLists.txt

--- src/multiplayer/chatui.cpp
+++ src/multiplayer/chatui.cpp
@@ -15,9 +15,18 @@
 #include "../baseui.h"
 #include "../graphics.h"
 #include "../statustext_overlay.h"
+#include "../version.h"
 #include "game_multiplayer.h"
 #include "server.h"
 #include "strfnd.h"
+#include "cryptopp/pwdbased.h"
+#include "cryptopp/sha.h"
+#include "cryptopp/crc.h"
+#include "cryptopp/base64.h"
+#include "cryptopp/osrng.h"
+#include "cryptopp/filters.h"
+#include "cryptopp/aes.h"
+#include "cryptopp/gcm.h"
 
 class DrawableOnlineStatus : public Drawable {
 	Rect bounds;
@@ -866,7 +875,118 @@ void AddClientInfo(std::string message) {
 	AddLogEntry("[Client]: ", message, "", Messages::CV_LOCAL);
 }
 
+// https://www.cryptopp.com/wiki/PKCS5_PBKDF2_HMAC
+std::string GetPasswordHash(const std::string& password, unsigned int iterations = 600000) {
+	std::string salt = Version::GetVersionString();
+
+	CryptoPP::byte derived[CryptoPP::SHA256::DIGESTSIZE];
+
+	CryptoPP::PKCS5_PBKDF2_HMAC<CryptoPP::SHA256> pbkdf;
+	CryptoPP::byte unused = 0;
+
+	pbkdf.DeriveKey(derived, sizeof(derived), unused,
+		(CryptoPP::byte*)password.c_str(), password.length(),
+		(CryptoPP::byte*)salt.c_str(), salt.length(),
+		iterations, 0.0f);
+
+	std::string result;
+	CryptoPP::Base64Encoder encoder(new CryptoPP::StringSink(result), false);
+	encoder.Put(derived, sizeof(derived));
+	encoder.MessageEnd();
+
+	return std::move(result);
+}
+
+uint32_t StringToCRC32(std::string& value) {
+	std::string digest;
+	CryptoPP::CRC32C crc;
+	crc.Update((CryptoPP::byte*)&value[0], value.size());
+	digest.resize(crc.DigestSize());
+	crc.Final((CryptoPP::byte*)&digest[0]);
+	return *((uint32_t*)digest.data());
+}
+
+// https://cryptopp.com/wiki/Hash_Functions
+// https://cryptopp.com/wiki/Authenticated_Encryption
+std::string EncryptMessage(const std::string& password, const std::string& plain) {
+	CryptoPP::byte key[CryptoPP::AES::MAX_KEYLENGTH];
+	CryptoPP::SHA256().CalculateDigest(key, (CryptoPP::byte*)password.c_str(), password.length());
+
+	CryptoPP::AutoSeededRandomPool prng;
+	CryptoPP::byte iv[CryptoPP::AES::BLOCKSIZE];
+	prng.GenerateBlock(iv, CryptoPP::AES::BLOCKSIZE);
+
+	std::string cipher;
+	try {
+		CryptoPP::GCM<CryptoPP::AES>::Encryption encryptor;
+		encryptor.SetKeyWithIV(key, CryptoPP::AES::MAX_KEYLENGTH, iv, CryptoPP::AES::BLOCKSIZE);
+
+		CryptoPP::StringSource(plain, true,
+			new CryptoPP::AuthenticatedEncryptionFilter(encryptor,
+				new CryptoPP::StringSink(cipher)
+			)
+		);
+	} catch(const CryptoPP::Exception& e) {
+		Output::Debug("EncryptMessage exception: {}", e.what());
+	}
+
+	std::string result;
+	CryptoPP::Base64Encoder encoder(new CryptoPP::StringSink(result), false);
+	encoder.Put(iv, sizeof(iv));
+	encoder.Put((CryptoPP::byte*)cipher.c_str(), cipher.length());
+	encoder.MessageEnd();
+
+	return std::move(result);
+}
+
+std::string DecryptMessage(const std::string& password, const std::string& data, std::string& recovered) {
+	CryptoPP::byte key[CryptoPP::AES::MAX_KEYLENGTH];
+	CryptoPP::SHA256().CalculateDigest(key, (CryptoPP::byte*)password.c_str(), password.length());
+
+	CryptoPP::byte iv[CryptoPP::AES::BLOCKSIZE];
+	std::string cipher;
+
+	CryptoPP::Base64Decoder decoder;
+	decoder.Put((CryptoPP::byte*)data.data(), data.size());
+	decoder.MessageEnd();
+
+	CryptoPP::word64 size = decoder.MaxRetrievable();
+	decoder.Get(iv, CryptoPP::AES::BLOCKSIZE);
+	size_t cipher_size = size-CryptoPP::AES::BLOCKSIZE;
+	cipher.resize(cipher_size);
+	decoder.Get((CryptoPP::byte*)cipher.data(), cipher_size);
+
+	std::string exception_what;
+	try {
+		CryptoPP::GCM<CryptoPP::AES>::Decryption decryptor;
+		decryptor.SetKeyWithIV(key, CryptoPP::AES::MAX_KEYLENGTH, iv, CryptoPP::AES::BLOCKSIZE);
+
+		CryptoPP::StringSource(cipher, true,
+			new CryptoPP::AuthenticatedDecryptionFilter(decryptor,
+				new CryptoPP::StringSink(recovered)
+			)
+		);
+	} catch(const CryptoPP::Exception& e) {
+		recovered = "";
+		exception_what = e.what();
+		Output::Debug("DecryptMessage exception: {}", exception_what);
+	}
+
+	return std::move(exception_what);
+}
+
 void InitHello() {
+	std::string k1 = "!! • IME input now supported!";
+	std::string k = GetPasswordHash(k1);
+	Output::Debug("--- key:{} salt:{}", k1, Version::GetVersionString());
+	Output::Debug("--- key hash {} {}", StringToCRC32(k), k);
+	std::string m = "• Type !help to list commands.";
+	std::string out = EncryptMessage(k, m);
+	Output::Debug("--- cipher {}", out);
+	std::string message;
+	std::string error = DecryptMessage(k, out, message);
+	Output::Debug("--- message {}; {}", error, message);
+
 	AddLogEntry("", "!! • IME input now supported!", "", Messages::CV_LOCAL);
 	AddLogEntry("", "!!   (for Japanese, etc.)", "", Messages::CV_LOCAL);
 	AddLogEntry("", "!! • You can now copy and", "", Messages::CV_LOCAL);
