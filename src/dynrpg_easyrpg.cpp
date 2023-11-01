a74265ebdb48c28aa5d8a7a41e66dd6a82e79fdb
Mauro Junior <45118493+jetrotal@users.noreply.github.com>
2023-10-31 21:56:49 -0300

    DynRPG - easyrpg_raw: Numeric Parameters can call destiny constants
    implementation is a bit ugly, need help to clear this

--- src/dynrpg_easyrpg.cpp
+++ src/dynrpg_easyrpg.cpp
@@ -134,8 +134,24 @@ bool DynRpg::EasyRpgPlugin::EasyRaw(dyn_arg_list args, Game_Interpreter* interpr
 		}
 
 		for (size_t i = 2; i < args.size(); ++i) {
-			auto [int_arg] = DynRpg::ParseArgs<int>(func, args.subspan(i), &okay);
-			output_args.push_back(int_arg);
+
+			
+			auto currArg = args[i];
+
+			if (currArg.find("@") == 0) {
+				currArg = currArg.substr(1);
+				currArg = Constants.get("DestinyScript", currArg);
+
+				auto int_arg = stoi(currArg);
+				okay = true;
+				output_args.push_back(int_arg);
+			}
+			else {
+				auto [int_arg] = DynRpg::ParseArgs<int>(func, args.subspan(i), &okay);
+				output_args.push_back(int_arg);
+			}
+
+			
 			if (!okay) {
 				return true;
 			}
