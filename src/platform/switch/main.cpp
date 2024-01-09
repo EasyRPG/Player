d4b7e8e3654cf88eef323667b10ed239fb039733
Carsten Teibes <dev@f4ke.de>
2024-01-09 01:18:13 +0100

    NX: fix copy/paste error in custom logging func

--- src/platform/switch/main.cpp
+++ src/platform/switch/main.cpp
@@ -28,9 +28,10 @@ namespace {
 }
 
 static void LogCallback(LogLevel lvl, std::string const& msg, LogCallbackUserData /* userdata */) {
+	std::string prefix = Output::LogLevelToString(lvl);
+
 	if (!is_nro) {
-		std::string m = std::string("[" GAME_TITLE "] ") +
-			Output::LogLevelToString(lvl) + ": " + msg;
+		std::string m = std::string("[" GAME_TITLE "] ") + prefix + ": " + msg;
 
 		// HLE in yuzu emulator
 		svcOutputDebugString(m.c_str(), m.length());
@@ -38,7 +39,7 @@ static void LogCallback(LogLevel lvl, std::string const& msg, LogCallbackUserDat
 
 	// additional to nxlink server
 	if(nxlinkSocket >= 0) {
-		printf("%s: %s\n", prefix.c_str(), message.c_str());
+		printf("%s: %s\n", prefix.c_str(), msg.c_str());
 	}
 }
 
