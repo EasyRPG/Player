ab232d15699d6bc87dfef3e9ff4985bc502d346c
florianessl <github@iessl.at>
2024-03-03 16:25:54 +0100

    Simple implementation for AntiLagSwitch

--- src/game_map.cpp
+++ src/game_map.cpp
@@ -1512,6 +1512,10 @@ void Game_Map::SetPositionY(int y, bool reset_panorama) {
 }
 
 bool Game_Map::GetNeedRefresh() {
+	if (Player::game_config.patch_anti_lag_switch.Get() && Main_Data::game_switches->Get(Player::game_config.patch_anti_lag_switch.Get())) {
+		return false;
+	}
+
 	return need_refresh;
 }
 
