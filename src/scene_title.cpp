e5f2930c0a5922bebe71ecda64b4f1079ff9e37b
Ghabry <gabriel+github@mastergk.de>
2024-03-11 20:39:52 +0100

    Title scene: Allow accessing the load scene for file upload in emscripten with Shift

--- src/scene_title.cpp
+++ src/scene_title.cpp
@@ -179,6 +179,12 @@ void Scene_Title::vUpdate() {
 			int index = translate_window->GetIndex();
 			ChangeLanguage(lang_dirs.at(index));
 		}
+	} else if (Input::IsTriggered(Input::SHIFT)) {
+		// For emscripten: Allow accessing the load scene for file upload with Shift
+		int index = command_window->GetIndex();
+		if (index == indices.continue_game) {
+			CommandContinue();
+		}
 	} else if (Input::IsTriggered(Input::CANCEL)) {
 		if (active_window == 1) {
 			// Switch back
@@ -353,7 +359,7 @@ void Scene_Title::CommandNewGame() {
 }
 
 void Scene_Title::CommandContinue() {
-	if (continue_enabled) {
+	if (continue_enabled || Input::IsTriggered(Input::SHIFT)) {
 		Main_Data::game_system->SePlay(Main_Data::game_system->GetSystemSE(Main_Data::game_system->SFX_Decision));
 	} else {
 		Main_Data::game_system->SePlay(Main_Data::game_system->GetSystemSE(Main_Data::game_system->SFX_Buzzer));
