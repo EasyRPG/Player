158131943f3738bbf045d4b036d1185cafa5c746
Ghabry <gabriel+github@mastergk.de>
2024-05-24 15:41:36 +0200

    Android: Fetch the game name from the INI
    The title is then reencoded based on the users encoding setting.

--- builds/android/app/src/main/java/org/easyrpg/player/game_browser/Game.java
+++ builds/android/app/src/main/java/org/easyrpg/player/game_browser/Game.java
@@ -18,6 +18,8 @@ public class Game implements Comparable<Game> {
     final static char escapeCode = '\u0001';
     /** The title shown in the Game Browser */
     private String title;
+    /** Bytes of the title string in an unspecified encoding */
+    private byte[] titleRaw = null;
     /** Path to the game folder (forwarded via --project-path */
     private final String gameFolderPath;
     /** Relative path to the save directory, made absolute by launchGame (forwarded via --save-path) */
@@ -29,15 +31,13 @@ public class Game implements Comparable<Game> {
     /** Game is launched from the APK via standalone mode */
     private boolean standalone = false;
 
-    public Game(String gameFolderPath, String saveFolder, String title, byte[] titleScreen) {
+    public Game(String gameFolderPath, String saveFolder, byte[] titleScreen) {
         this.gameFolderPath = gameFolderPath;
 
         // is only relative here, launchGame will put this in the "saves" directory
         if (!saveFolder.isEmpty()) {
             savePath = saveFolder;
         }
-
-        this.title = title;
 
         if (titleScreen != null) {
             this.titleScreen = BitmapFactory.decodeByteArray(titleScreen, 0, titleScreen.length);
@@ -46,32 +46,15 @@ public class Game implements Comparable<Game> {
         this.isFavorite = isFavoriteFromSettings();
     }
 
-    public static Game fromCacheEntry(Context context, String cache) {
-        String[] entries = cache.split(String.valueOf(escapeCode));
-
-        if (entries.length != 4) {
-            return null;
-        }
-
-        String savePath = entries[0];
-        DocumentFile gameFolder = DocumentFile.fromTreeUri(context, Uri.parse(entries[1]));
-        if (gameFolder == null) {
-            return null;
-        }
-
-        String title = entries[2];
-
-        byte[] titleScreen = null;
-        if (!entries[3].equals("null")) {
-            titleScreen = Base64.decode(entries[3], 0);
-        }
-
-        return new Game(entries[1], savePath, title, titleScreen);
-    }
-
     public String getTitle() {
         return title;
     }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public native void reencodeTitle();
 
     public String getGameFolderPath() {
         return gameFolderPath;
@@ -113,41 +96,29 @@ public class Game implements Comparable<Game> {
         return this.title.compareTo(game.title);
     }
 
+    /**
+     * Returns a unique key to be used for storing settings related to the game.
+     *
+     * @return unique key
+     */
+    public String getKey() {
+        return gameFolderPath.replaceAll("[/ ]", "_");
+    }
+
     public Encoding getEncoding() {
         return SettingsManager.getGameEncoding(this);
     }
 
     public void setEncoding(Encoding encoding) {
         SettingsManager.setGameEncoding(this, encoding);
+        reencodeTitle();
     }
 
-    @NonNull
-    @Override
-    public String toString() {
-        return getTitle();
-    }
-
-    public String toCacheEntry() {
-        StringBuilder sb = new StringBuilder();
-
-        // Cache structure: savePath | gameFolderPath | title | titleScreen
-        sb.append(savePath);
-        sb.append(escapeCode);
-        sb.append(gameFolderPath);
-        sb.append(escapeCode);
-        sb.append(title);
-        sb.append(escapeCode);
-
-        if (titleScreen != null) {
-            ByteArrayOutputStream baos = new ByteArrayOutputStream();
-            titleScreen.compress(Bitmap.CompressFormat.PNG, 90, baos);
-            byte[] b = baos.toByteArray();
-            sb.append(Base64.encodeToString(b, Base64.NO_WRAP));
-        } else {
-            sb.append("null");
-        }
-
-        return sb.toString();
+    /**
+     * @return The encoding number or "auto" when not configured (for use via JNI)
+     */
+    public String getEncodingCode() {
+        return getEncoding().getRegionCode();
     }
 
     public Bitmap getTitleScreen() {
@@ -160,5 +131,75 @@ public class Game implements Comparable<Game> {
 
     public void setStandalone(boolean standalone) {
         this.standalone = standalone;
+    }
+
+    @NonNull
+    @Override
+    public String toString() {
+        return getTitle();
+    }
+
+    public static Game fromCacheEntry(Context context, String cache) {
+        String[] entries = cache.split(String.valueOf(escapeCode));
+
+        if (entries.length != 5) {
+            return null;
+        }
+
+        String savePath = entries[0];
+        DocumentFile gameFolder = DocumentFile.fromTreeUri(context, Uri.parse(entries[1]));
+        if (gameFolder == null) {
+            return null;
+        }
+
+        String title = entries[2];
+
+        byte[] titleRaw = null;
+        if (!entries[3].equals("null")) {
+            titleRaw = Base64.decode(entries[3], 0);
+        }
+
+        byte[] titleScreen = null;
+        if (!entries[4].equals("null")) {
+            titleScreen = Base64.decode(entries[4], 0);
+        }
+
+        Game g = new Game(entries[1], savePath, titleScreen);
+        g.setTitle(title);
+        g.titleRaw = titleRaw;
+
+        if (g.titleRaw != null) {
+            g.reencodeTitle();
+        }
+
+        return g;
+    }
+
+    public String toCacheEntry() {
+        StringBuilder sb = new StringBuilder();
+
+        // Cache structure: savePath | gameFolderPath | title | titleRaw | titleScreen
+        sb.append(savePath);
+        sb.append(escapeCode);
+        sb.append(gameFolderPath);
+        sb.append(escapeCode);
+        sb.append(title);
+        sb.append(escapeCode);
+        if (titleRaw != null) {
+            sb.append(Base64.encodeToString(titleRaw, Base64.NO_WRAP));
+        } else {
+            sb.append("null");
+        }
+        sb.append(escapeCode);
+        if (titleScreen != null) {
+            ByteArrayOutputStream baos = new ByteArrayOutputStream();
+            titleScreen.compress(Bitmap.CompressFormat.PNG, 90, baos);
+            byte[] b = baos.toByteArray();
+            sb.append(Base64.encodeToString(b, Base64.NO_WRAP));
+        } else {
+            sb.append("null");
+        }
+
+        return sb.toString();
     }
 }
