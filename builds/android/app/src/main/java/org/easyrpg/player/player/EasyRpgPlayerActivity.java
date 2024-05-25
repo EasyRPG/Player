eb339f4f6c8be9d8401e9e715a9ad9c880ce5812
Ghabry <gabriel+github@mastergk.de>
2024-05-26 01:21:43 +0200

    Android: Add "Toggle FPS" and "Reset Game" to the game drawer
    Related #3103

--- builds/android/app/src/main/java/org/easyrpg/player/player/EasyRpgPlayerActivity.java
+++ builds/android/app/src/main/java/org/easyrpg/player/player/EasyRpgPlayerActivity.java
@@ -181,10 +181,14 @@ public class EasyRpgPlayerActivity extends SDLActivity implements NavigationView
         } else if (item.getItemId() == R.id.toggle_ui) {
             uiVisible = !uiVisible;
             showInputLayout();
+        } else if (item.getItemId() == R.id.toggle_fps) {
+            toggleFps();
         } else if (item.getItemId() == R.id.edit_layout) {
             editLayout();
         } else if (item.getItemId() == R.id.report_bug) {
             reportBug();
+        } else if (item.getItemId() == R.id.reset_game) {
+            showResetGameDialog();
         } else if (item.getItemId() == R.id.end_game) {
             showEndGameDialog();
         }
@@ -312,6 +316,20 @@ public class EasyRpgPlayerActivity extends SDLActivity implements NavigationView
         }
     }
 
+    private void showResetGameDialog() {
+        AlertDialog.Builder alertDialogBuilder = new AlertDialog.Builder(this);
+        alertDialogBuilder.setTitle(R.string.app_name);
+
+        // set dialog message
+        alertDialogBuilder.setMessage(R.string.do_want_reset).setCancelable(false)
+            .setPositiveButton(R.string.yes, (dialog, id) -> resetGame()).setNegativeButton(R.string.no, (dialog, id) -> dialog.cancel());
+
+        // create alert dialog
+        AlertDialog alertDialog = alertDialogBuilder.create();
+
+        alertDialog.show();
+    }
+
     private void showEndGameDialog() {
         AlertDialog.Builder alertDialogBuilder = new AlertDialog.Builder(this);
         alertDialogBuilder.setTitle(R.string.app_name);
@@ -330,7 +348,9 @@ public class EasyRpgPlayerActivity extends SDLActivity implements NavigationView
 
     public static native void endGame();
 
-    public static native void setFastForwardMultiplier(int m);
+    public static native void resetGame();
+
+    public static native void toggleFps();
 
     protected String[] getArguments() {
         return getIntent().getStringArrayExtra(TAG_COMMAND_LINE);
