764fbf364eb78722ad62e65660b9bc6a0a139a49
Ghabry <gabriel+github@mastergk.de>
2025-02-10 19:34:57 +0100

    CMake: Remove code that depends on CMake < 3.16
    Is unsupported since a while.

--- builds/cmake/Modules/PlayerConfigureWindows.cmake
+++ builds/cmake/Modules/PlayerConfigureWindows.cmake
@@ -13,42 +13,8 @@ if(WIN32)
 endif()
 
 if(MSVC)
-	if(${CMAKE_VERSION} VERSION_LESS "3.15.0") 
-		message(WARNING "Your CMake version is older than 3.15")
-		message(WARNING "For proper MSVC runtime library support upgrade to a newer version")
-		
-		option(SHARED_RUNTIME "Windows: Build using the shared runtime library (/MD), disable for static runtime (/MT)" ON)
-		
-		# Set compiler options.
-		set(variables
-			CMAKE_C_FLAGS_DEBUG
-			CMAKE_C_FLAGS_MINSIZEREL
-			CMAKE_C_FLAGS_RELEASE
-			CMAKE_C_FLAGS_RELWITHDEBINFO
-			CMAKE_CXX_FLAGS_DEBUG
-			CMAKE_CXX_FLAGS_MINSIZEREL
-			CMAKE_CXX_FLAGS_RELEASE
-			CMAKE_CXX_FLAGS_RELWITHDEBINFO
-		)
-		if(SHARED_RUNTIME)
-			message(STATUS "Windows: Using dynamic runtime library (/MD)")
-			foreach(variable ${variables})
-				if(${variable} MATCHES "/MT")
-					string(REGEX REPLACE "/MT" "/MD" ${variable} "${${variable}}")
-				endif()
-			endforeach()
-		else()
-			message(STATUS "Windows: Using static runtime library (/MT)")
-			foreach(variable ${variables})
-				if(${variable} MATCHES "/MD")
-					string(REGEX REPLACE "/MD" "/MT" ${variable} "${${variable}}")
-				endif()
-			endforeach()
-		endif()
-	else()
-		# Depends on vcpkg but we don't support anything else
-		set(CMAKE_MSVC_RUNTIME_LIBRARY "MultiThreaded$<$<CONFIG:Debug>:Debug>$<$<STREQUAL:${VCPKG_CRT_LINKAGE},dynamic>:DLL>" CACHE STRING "")
-	endif()
+	# Depends on vcpkg but we don't support anything else
+	set(CMAKE_MSVC_RUNTIME_LIBRARY "MultiThreaded$<$<CONFIG:Debug>:Debug>$<$<STREQUAL:${VCPKG_CRT_LINKAGE},dynamic>:DLL>" CACHE STRING "")
 
 	option(MSVC_MULTICORE "MSVC: Build using multiple cores (/MP)" ON)
 	if (MSVC_MULTICORE)
