7a08373b1ad69738a882d94eea2c5fc790bfdb5a
Ghabry <gabriel+github@mastergk.de>
2026-02-21 19:02:18 +0100

    Implement saving of Canvas bitmaps
    Most compatible with what Maniacs is doing
    At least we can load Maniacs images but Maniacs shows nothing

--- src/game_pictures.h
+++ src/game_pictures.h
@@ -20,7 +20,6 @@
 
 // Headers
 #include <string>
-#include <deque>
 #include "async_handler.h"
 #include <lcf/rpg/savepicture.h>
 #include "sprite_picture.h"
@@ -127,6 +126,8 @@ public:
 		void OnMapScrolled(int dx, int dy);
 
 		void AttachWindow(const Window_Base& window);
+		void LoadCanvas();
+		lcf::rpg::SavePicture OnSave() const;
 
 		bool IsNormalPicture() const;
 		bool IsWindowAttached() const;
